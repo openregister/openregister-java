945a06d37d41b46974f8facb8565038e62d1307c
omsharma <foromsharma@gmail.com>
2015-06-22 15:55:37 +0100

    * Added documentation in README.md * building a fat jar, might not be required later * Added functionality to pick configuration file name as program parameter

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mint
 A repository for mint project
+
+# Requirements
+
+- Java 1.8+
+- Rabbit MQ 3.5.1
+- Postgres DB 9.4
+
+# Build and Run project
+
+- Install and run Rabbit MQ
+- Install and run postgres db
+- Create database mint `createdb mint` to use application locally
+- Create database mint `createdb test_mint` to run unit tests
+- Checkout project 
+- Use command `./gradlew cleanIdea idea` to generate the idea project files
+- Build project using command `./gradlew clean build` 
--- src/main/java/uk/gov/Application.java
+++ src/main/java/uk/gov/Application.java
@@ -6,12 +6,16 @@ import org.apache.commons.lang.StringUtils;
 import uk.gov.store.DataStore;
 import uk.gov.store.PostgresDataStore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Application {
 
     public static void main(String[] args) throws ConfigurationException, InterruptedException {
 
-        //TODO: use application arguments to take properties
-        String fileName = null;
+        Map<String, String> propertiesMap = createConfigurationMap(args);
+
+        String fileName = propertiesMap.get("config.file");
 
         if (StringUtils.isEmpty(fileName)) {
             System.out.println("Properties file name not provided, using default application.properties file");
@@ -31,6 +35,17 @@ public class Application {
         System.out.println("Application started...");
 
         Thread.currentThread().join();
+    }
+
+    private static Map<String, String> createConfigurationMap(String[] args) {
+        Map<String, String> appParams = new HashMap<>();
+        for (int i = 0; args != null && i < args.length; i++) {
+            if (args[i].contains("=")) {
+                String[] kv = args[i].split("=", 2);
+                appParams.put(kv[0], kv[1]);
+            }
+        }
+        return appParams;
     }
 
 }
