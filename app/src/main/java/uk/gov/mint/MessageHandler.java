529701df3386d88b94ec8b49bf58cd3b4b85623d
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-07-06 16:58:06 +0100

    reject messages which are not well-formed JSON
    This updates the mint to reject messages which are not well-formed JSON.
    
    Already the complexity of the message queue is rearing its head as we
    need to decide what to do when a message is not well-formed: we want a
    dead letter queue of some sort.  And how do we report back to the
    original sender that there was a problem?
    
    A better solution would be to remove RabbitMQ and make the entry
    submission endpoint synchronous; the reason I haven't done this here is
    it requires rewriting the bulk import script.

--- app/src/main/java/uk/gov/mint/MessageHandler.java
+++ app/src/main/java/uk/gov/mint/MessageHandler.java
@@ -1,5 +1,8 @@
 package uk.gov.mint;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
@@ -12,15 +15,26 @@ class MessageHandler extends DefaultConsumer {
 
     private final Channel channel;
     private final DataStoreApplication dataStoreApplication;
+    private final ObjectMapper objectMapper;
 
     public MessageHandler(Channel channel, DataStoreApplication dataStoreApplication) {
         super(channel);
         this.channel = channel;
         this.dataStoreApplication = dataStoreApplication;
+        objectMapper = new ObjectMapper();
     }
 
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        try {
+            objectMapper.readValue(body, JsonNode.class);
+        } catch (JsonParseException e) {
+            // ACK message as we know it's unprocessable
+            channel.basicAck(envelope.getDeliveryTag(), false);
+            // FIXME: should report this failure somewhere
+            return;
+        }
+
         dataStoreApplication.add(body);
         channel.basicAck(envelope.getDeliveryTag(), false);
     }
--- app/src/test/java/uk/gov/mint/MessageHandlerTest.java
+++ app/src/test/java/uk/gov/mint/MessageHandlerTest.java
@@ -11,6 +11,7 @@ import uk.gov.integration.DataStoreApplication;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -37,5 +38,35 @@ public class MessageHandlerTest {
 
         verify(dataStore).add(jsonBytes);
         verify(channel).basicAck(52L, false);
+    }
+
+    @Test
+    public void shouldNotAcceptIllFormedJsonEntry() throws Exception {
+        byte[] jsonBytes = "{\"foo\":\"bar\"".getBytes();
+
+        Envelope mock = mock(Envelope.class);
+        when(mock.getDeliveryTag()).thenReturn(52L);
+
+        messageHandler.handleDelivery(null, mock, null, jsonBytes);
+
+        // We should ack the message as it's ill-formed
+        // TODO: need some sort of dead letter queue
+        verify(channel).basicAck(52L, false);
+
+        verifyNoMoreInteractions(channel, dataStore);
+    }
+
+    @Test
+    public void shouldNotAcceptJsonEntryWithUnescapedQuoteMarks() throws Exception {
+        byte[] jsonBytes = "{\"foo\":\"\"\"}".getBytes();
+        Envelope mock = mock(Envelope.class);
+        when(mock.getDeliveryTag()).thenReturn(52L);
+        messageHandler.handleDelivery(null, mock, null, jsonBytes);
+
+        // We should ack the message as it's ill-formed
+        // TODO: need some sort of dead letter queue
+        verify(channel).basicAck(52L, false);
+
+        verifyNoMoreInteractions(channel, dataStore);
     }
 }
