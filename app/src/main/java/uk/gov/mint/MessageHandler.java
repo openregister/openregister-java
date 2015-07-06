e15f4ecda97ca5f60f8d344bc6160c651400efd0
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-07-06 18:31:41 +0100

    convert JSON to canonical form
    We need to ensure that JSON is converted to a single canonical form, so
    that any hashes we perform on it consistently give the same value for
    the same semantic JSON content.
    
    This commit ensures:
    
      - nonsignificant whitespace is trimmed
      - key order is consistent
      - unicode escape sequences are not used
    
    This was loosely based on OLPC's consistent JSON format [1] except that
    OLPC-Canonical-JSON requires newlines to be literal newlines, which is
    actually banned by JSON as newlines are a control character (!).  So we
    use escaped newlines instead.
    
    If we want to be /really/ serious, we should probably use NFC
    canonicalization on the Unicode strings too (!).  But this commit
    doesn't attempt this.
    
    [1]: http://wiki.laptop.org/go/Canonical_JSON

--- app/src/main/java/uk/gov/mint/MessageHandler.java
+++ app/src/main/java/uk/gov/mint/MessageHandler.java
@@ -1,8 +1,11 @@
 package uk.gov.mint;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.DefaultConsumer;
@@ -10,6 +13,8 @@ import com.rabbitmq.client.Envelope;
 import uk.gov.integration.DataStoreApplication;
 
 import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 
 class MessageHandler extends DefaultConsumer {
 
@@ -22,12 +27,15 @@ class MessageHandler extends DefaultConsumer {
         this.channel = channel;
         this.dataStoreApplication = dataStoreApplication;
         objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+        objectMapper.getFactory().configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, false);
     }
 
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        JsonNode jsonNode;
         try {
-            objectMapper.readValue(body, JsonNode.class);
+            jsonNode = objectMapper.readValue(body, JsonNode.class);
         } catch (JsonParseException e) {
             // ACK message as we know it's unprocessable
             channel.basicAck(envelope.getDeliveryTag(), false);
@@ -35,7 +43,15 @@ class MessageHandler extends DefaultConsumer {
             return;
         }
 
-        dataStoreApplication.add(body);
+        dataStoreApplication.add(canonicalize(jsonNode));
         channel.basicAck(envelope.getDeliveryTag(), false);
+    }
+
+    private byte[] canonicalize(JsonNode jsonNode) throws JsonProcessingException {
+        // Method from http://stackoverflow.com/questions/18952571/jackson-jsonnode-to-string-with-sorted-keys
+        Object obj = objectMapper.treeToValue(jsonNode, Object.class);
+        Writer w = new StringWriter();
+        // for some reason, writeValueAsString(obj).getBytes() doesn't re-escape unicode, but writeValueAsBytes does
+        return objectMapper.writeValueAsString(obj).getBytes();
     }
 }
