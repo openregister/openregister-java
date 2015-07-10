a4601cfbf3d18e449be0c3f12d39200ea39d724d
omsharma <foromsharma@gmail.com>
2015-07-10 15:30:39 +0100

    PAul specifically asked to have a integration test for CORS header

--- src/test/java/uk/gov/register/presentation/functional/FunctionalTest.java
+++ src/test/java/uk/gov/register/presentation/functional/FunctionalTest.java
@@ -1,6 +1,7 @@
 package uk.gov.register.presentation.functional;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.net.HttpHeaders;
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
@@ -14,11 +15,13 @@ import uk.gov.register.presentation.app.PresentationApplication;
 import uk.gov.register.presentation.config.PresentationConfiguration;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertNotNull;
 
 public class FunctionalTest {
     public static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/ft_presentation";
@@ -39,7 +42,7 @@ public class FunctionalTest {
     private static Client client;
 
     @BeforeClass
-   public  static void beforeClass() throws InterruptedException {
+    public static void beforeClass() throws InterruptedException {
         client = new JerseyClientBuilder(RULE.getEnvironment()).build("test client");
         publishTestMessages();
     }
@@ -51,6 +54,26 @@ public class FunctionalTest {
 
         assertThat(response.readEntity(String.class), equalTo("[{\"key1\":\"key1Value_2\",\"ft_test_pkey\":\"ft_test_pkey_value_2\"},{\"key1\":\"key1Value_1\",\"ft_test_pkey\":\"ft_test_pkey_value_1\"}]"));
 
+    }
+
+    @Test
+    public void appSupportsCORS() {
+        String origin = "http://originfortest.com";
+        Response response = client.target(String.format("http://localhost:%d/latest", RULE.getLocalPort()))
+                .request()
+                .header(HttpHeaders.ORIGIN, origin)
+                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS, "X-Requested-With")
+                .options();
+
+
+        MultivaluedMap<String, Object> headers = response.getHeaders();
+
+        assertThat(headers.get(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN), equalTo(ImmutableList.of(origin)));
+        assertThat(headers.get(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS), equalTo((ImmutableList.of("true"))));
+        assertNotNull(headers.get(HttpHeaders.ACCESS_CONTROL_MAX_AGE));
+        assertThat(headers.get(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS), equalTo((ImmutableList.of("OPTIONS,GET,PUT,POST,DELETE,HEAD"))));
+        assertThat(headers.get(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS), equalTo((ImmutableList.of("X-Requested-With,Content-Type,Accept,Origin"))));
     }
 
     @Test
