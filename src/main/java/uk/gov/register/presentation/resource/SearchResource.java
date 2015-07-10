d6d6555d3ed12f2ddd69bc4fd5e3fa8a04265426
omsharma <foromsharma@gmail.com>
2015-07-10 12:50:43 +0100

    Removed unused HttpServletRequest Field

--- src/main/java/uk/gov/register/presentation/resource/SearchResource.java
+++ src/main/java/uk/gov/register/presentation/resource/SearchResource.java
@@ -4,20 +4,16 @@ import com.fasterxml.jackson.databind.JsonNode;
 import uk.gov.register.presentation.dao.PGObjectFactory;
 import uk.gov.register.presentation.dao.RecentEntryIndexQueryDAO;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/")
 public class SearchResource {
     private final RecentEntryIndexQueryDAO queryDAO;
-    @Context
-    protected HttpServletRequest httpServletRequest;
 
     public SearchResource(RecentEntryIndexQueryDAO queryDAO) {
         this.queryDAO = queryDAO;
