95e1988895d508758621f0b2739e0be812371407
Saqib Saleem (EE) <ssaleem@equalexperts.com>
2015-07-14 18:12:21 +0100

    Selective processing for result object if it is html.
    For html, convert to set of entries so that mustache will play nicely.

--- src/main/java/uk/gov/register/presentation/view/AbstractView.java
+++ src/main/java/uk/gov/register/presentation/view/AbstractView.java
@@ -2,12 +2,17 @@ package uk.gov.register.presentation.view;
 
 import io.dropwizard.views.View;
 
+import java.util.Map;
+import java.util.Set;
+
 public abstract class AbstractView extends View {
     protected AbstractView(String templateName) {
         super(templateName);
     }
 
     public abstract Object getObject();
+
+    public abstract ResultView flatten();
 }
 
 
