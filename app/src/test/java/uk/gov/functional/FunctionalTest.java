4546a8669735efc8097ddb7eee0d9ab33bf42561
Saqib Saleem (EE) <ssaleem@equalexperts.com>
2015-07-20 14:53:59 +0100

    Re-enabled FunctionalTest.

--- app/src/test/java/uk/gov/functional/FunctionalTest.java
+++ app/src/test/java/uk/gov/functional/FunctionalTest.java
@@ -1,10 +1,14 @@
 package uk.gov.functional;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.jcabi.http.Request;
+import com.jcabi.http.Response;
+import com.jcabi.http.request.JdkRequest;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import uk.gov.Application;
+import uk.gov.mint.CanonicalJsonMapper;
 import uk.gov.store.EntriesQueryDAO;
 import uk.gov.store.EntriesUpdateDAO;
 import uk.gov.store.HighWaterMarkDAO;
@@ -18,18 +22,20 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-@Ignore
 public class FunctionalTest {
     private java.sql.Connection pgConnection;
     private Application application;
     private TestKafkaCluster testKafkaCluster;
+    private Properties properties;
 
     @Before
     public void setup() throws Exception {
-        Properties properties = new Properties();
+        properties = new Properties();
         String configFilename = FunctionalTest.class.getResource("/test-application.properties").getFile();
         InputStream resourceAsStream = FunctionalTest.class.getResourceAsStream("/test-application.properties");
         properties.load(resourceAsStream);
@@ -52,17 +58,53 @@ public class FunctionalTest {
 
     @Test
     public void checkMessageIsConsumedAndStoredInDatabase() throws Exception {
+        CanonicalJsonMapper canonicalJsonMapper = new CanonicalJsonMapper();
 
-        String messageString = String.format("{\"time\":%s}", String.valueOf(System.currentTimeMillis()));
+        String messageString = String.format("{\"name\":\"ft_mint_test\",\"test\":\"test_%s\"}",
+                String.valueOf(System.currentTimeMillis()));
         byte[] message = messageString.getBytes();
-        // TODO: Change this to use HTTP connection
-//        connection.createChannel().basicPublish(exchange, routingKey, new AMQP.BasicProperties(), message);
+        JsonNode messageJson = canonicalJsonMapper.readFromBytes(message);
 
         waitForMessageToBeConsumed();
-        assertThat(tableRecord(), is(message));
+        send(
+                javaslang.collection.List.of(messageString)
+        );
+
+        waitForMessageToBeConsumed();
+
+        byte[] actual = tableRecord();
+        String actualString = new String(actual);
+        final JsonNode actualJson = canonicalJsonMapper.readFromBytes(actual);
+
+        JsonNode entryNode = actualJson.get("entry");
+        assertThat(actualJson.get("hash"), notNullValue(JsonNode.class));
+        assertThat(entryNode, notNullValue(JsonNode.class));
+
+        assertThat(entryNode.get("name").textValue(),
+                equalTo(messageJson.get("name").textValue()));
+        assertThat(entryNode.get("test").textValue(),
+                equalTo(messageJson.get("test").textValue()));
 
         List<String> messages = testKafkaCluster.readMessages("register", 1);
-        assertThat(messages, is(Collections.singletonList(messageString)));
+        assertThat(messages, is(Collections.singletonList(actualString)));
+    }
+
+    private void send(javaslang.collection.List<String> payload) {
+        try {
+            final String mintUrl = properties.getProperty("mintUrl");
+            Response r = new JdkRequest(mintUrl)
+                    .method(Request.POST)
+                    .body()
+                    .set(payload.join("\n"))
+                    .back()
+                    .fetch();
+            if (r.status() != 200)
+                System.err.println("Unexpected result: " + r.body());
+            else
+                System.out.println("Loaded " + 1000 + " entries...");
+        } catch (Exception e) {
+            System.err.println("Error occurred sending data to mint: " + e);
+        }
     }
 
     private byte[] tableRecord() throws SQLException {
