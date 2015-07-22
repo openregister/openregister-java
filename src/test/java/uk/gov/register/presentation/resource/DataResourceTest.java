b90424f512d68e289ca3d390a9e3f28dcec2d11a
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-07-22 10:46:31 +0100

    Ensure /feed supports csv and tsv

---
+++ src/test/java/uk/gov/register/presentation/resource/DataResourceTest.java
@@ -0,0 +1,26 @@
+package uk.gov.register.presentation.resource;
+
+import org.junit.Test;
+import uk.gov.register.presentation.representations.ExtraMediaType;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
+
+public class DataResourceTest {
+    @Test
+    public void feedSupportsJsonCsvTsv() throws Exception {
+        Method feedMethod = DataResource.class.getDeclaredMethod("feed");
+        List<String> declaredMediaTypes = asList(feedMethod.getAnnotation(Produces.class).value());
+        assertThat(declaredMediaTypes, hasItems(
+                MediaType.APPLICATION_JSON,
+                ExtraMediaType.TEXT_CSV,
+                ExtraMediaType.TEXT_TSV
+        ));
+    }
+}
