0a7741abec8e6cb538de34141fa0602459391059
omsharma <foromsharma@gmail.com>
2015-08-03 16:36:00 +0100

    added deploy block and scripts to deploy mint using code deploy
    resolved the copy paste bug
    
    changed the secret key

---
+++ app/deploy/scripts/start-service.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker run -p 4567:4567 \
+    --volume /srv/mint:/srv/mint \
+    --link etc_postgres_1:postgres \
+    --link etc_kafka_1:kafka \
+    jstepien/openjdk8 java -jar /srv/mint/mint.jar
