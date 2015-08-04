8c60308a187c1f9eee328e79d1c47508fe307e09
omsharma <foromsharma@gmail.com>
2015-08-04 15:18:22 +0100

    stop/start using docker in aws environment validate that the service started successfully

--- deploy/scripts/stop-service.sh
+++ deploy/scripts/stop-service.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -x
-RES=`ps aux | grep -v grep | grep 'presentation.jar' | awk '{print $2}'`
-if [[ ! -z "$RES" ]]; then
-    kill $RES
-fi
+docker stop presentationApp
+docker rm presentationApp
--- deploy/scripts/validate.sh
+++ deploy/scripts/validate.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-#curl -q http://registerurl/ should return 200
+set -e
+RES=`curl -s -o /dev/null -w "%{http_code}" http://localhost/`
+if [[ "$RES" != "200" ]]; then
+    echo "Error: can not curl the application"
+    exit 255;
+fi
