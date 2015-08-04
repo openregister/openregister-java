75f3436e2fa5827b7003e9f9d8ad64dde9500411
omsharma <foromsharma@gmail.com>
2015-08-04 15:28:59 +0100

    start/stop application using docker in aws environment

--- app/deploy/scripts/start-service.sh
+++ app/deploy/scripts/start-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker run -d -p 4567:4567 \
+docker run -d --name=mintApp -p 4567:4567 \
     --volume /srv/mint:/srv/mint \
     --link etc_postgres_1:postgres \
     --link etc_kafka_1:kafka \
