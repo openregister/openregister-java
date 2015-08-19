5c841ee3074063d02f7c832df8a7c19db801a185
omsharma <foromsharma@gmail.com>
2015-08-19 17:04:27 +0100

    Added 404 and 500 pages. 404.html is rendered for NotFoundException while 500.html is rendered for any other exception caught.

---
+++ src/main/java/uk/gov/register/presentation/resource/NotFoundExceptionMapper.java
@@ -0,0 +1,19 @@
+package uk.gov.register.presentation.resource;
+
+import uk.gov.register.thymeleaf.ThymeleafView;
+
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class NotFoundExceptionMapper extends ResourceBase implements ExceptionMapper<NotFoundException> {
+    public Response toResponse(NotFoundException exception) {
+        return Response.status(Response.Status.NOT_FOUND)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML)
+                .entity(new ThymeleafView(httpServletRequest, httpServletResponse, servletContext, "404.html") {
+                })
+                .build();
+    }
+}
---
+++ src/main/java/uk/gov/register/presentation/resource/ThrowableExceptionMapper.java
@@ -0,0 +1,19 @@
+package uk.gov.register.presentation.resource;
+
+import uk.gov.register.thymeleaf.ThymeleafView;
+
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class ThrowableExceptionMapper extends ResourceBase implements ExceptionMapper<Throwable> {
+    @Override
+    public Response toResponse(Throwable exception) {
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML)
+                .entity(new ThymeleafView(httpServletRequest, httpServletResponse, servletContext, "500.html") {
+                })
+                .build();
+    }
+}
