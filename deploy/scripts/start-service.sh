cf22f4b4bff0e63c1fe720c4d24ccb19cc0a9b49
omsharma <foromsharma@gmail.com>
2015-08-25 11:08:29 +0100

    changed start-service to start indexer app as docker container

--- deploy/scripts/start-service.sh
+++ deploy/scripts/start-service.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 aws s3 cp s3://preview.config/indexer/indexer.properties /srv/indexer --region eu-west-1
-docker run -d --name=indexerApp -p 4567:4567 \
+docker run -d --name=indexerApp \
     --volume /srv/indexer:/srv/indexer \
-    --link etc_kafka_1:kafka \
     jstepien/openjdk8 java -jar /srv/indexer/indexer.jar config.file=/srv/indexer/indexer.properties
