321c2a311863ee82191d81a56f65baee8173b648
omsharma <foromsharma@gmail.com>
2015-08-26 12:05:41 +0100

    parameterized config name based on instance tag

--- deploy/scripts/start-service.sh
+++ deploy/scripts/start-service.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-aws s3 cp s3://preview.config/country/presentation/config.yaml /srv/presentation --region eu-west-1
+INSTANCE_ID=$(curl 169.254.169.254/2014-11-05/meta-data/instance-id)
+export AWS_DEFAULT_REGION=eu-west-1
+REGISTER_NAME=$(aws ec2 describe-tags --filters Name=resource-id,Values=$INSTANCE_ID Name=key,Values=Name --query 'Tags[0].Value' --output text)
+
+aws s3 cp s3://preview.config/${REGISTER_NAME}/presentation/config.yaml /srv/presentation --region eu-west-1
 docker run --name=presentationApp -d -p 80:8080 --volume /srv/presentation:/srv/presentation \
     jstepien/openjdk8 java -jar /srv/presentation/presentation.jar server /srv/presentation/config.yaml
