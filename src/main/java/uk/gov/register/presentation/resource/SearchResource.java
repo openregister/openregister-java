75ff633f5dd1c6cc7ab0977061752fb71a5d64e8
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-09-14 11:53:59 +0100

    remove duplicate logic

--- src/main/java/uk/gov/register/presentation/resource/SearchResource.java
+++ src/main/java/uk/gov/register/presentation/resource/SearchResource.java
@@ -30,30 +30,32 @@ public class SearchResource {
     @Path("/{primaryKey}/{primaryKeyValue}")
     @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON, ExtraMediaType.TEXT_YAML, ExtraMediaType.TEXT_CSV, ExtraMediaType.TEXT_TSV, ExtraMediaType.TEXT_TTL})
     public SingleResultView findByPrimaryKey(@PathParam("primaryKey") String key, @PathParam("primaryKeyValue") String value) {
-        if (key.equals(requestContext.getRegisterPrimaryKey())) {
-            Optional<DbRecord> record = queryDAO.findByKeyValue(key, value);
-            if (record.isPresent()) {
-                setVersionHistoryLinkHeader(record.get());
-                return viewFactory.getSingleResultView(record.get());
-            }
+        if (!key.equals(requestContext.getRegisterPrimaryKey())) {
+            throw new NotFoundException();
         }
-
-        throw new NotFoundException();
+        Optional<DbRecord> recordO = queryDAO.findByKeyValue(key, value);
+        return recordResponse(recordO);
     }
 
     @GET
     @Path("/hash/{hash}")
     @Produces({MediaType.TEXT_HTML, MediaType.APPLICATION_JSON, ExtraMediaType.TEXT_YAML, ExtraMediaType.TEXT_CSV, ExtraMediaType.TEXT_TSV, ExtraMediaType.TEXT_TTL})
     public SingleResultView findByHash(@PathParam("hash") String hash) {
-        Optional<DbRecord> optionalRecord = queryDAO.findByHash(hash);
-        if (optionalRecord.isPresent()) {
-            DbRecord record = optionalRecord.get();
+        Optional<DbRecord> recordO = queryDAO.findByHash(hash);
+        return recordResponse(recordO);
+    }
 
-            setVersionHistoryLinkHeader(record);
+    @GET
+    @Path("/entry/{serial}")
+    public SingleResultView findBySerial(@PathParam("serial") int serial) {
+        Optional<DbRecord> recordO = queryDAO.findBySerial(serial);
+        return recordResponse(recordO);
+    }
 
-            return viewFactory.getSingleResultView(record);
-        }
-        throw new NotFoundException();
+    private SingleResultView recordResponse(Optional<DbRecord> optionalRecord) {
+        optionalRecord.ifPresent(this::setVersionHistoryLinkHeader);
+        return optionalRecord.map(viewFactory::getSingleResultView)
+                .orElseThrow(NotFoundException::new);
     }
 
     private void setVersionHistoryLinkHeader(DbRecord record) {
@@ -63,14 +65,5 @@ public class SearchResource {
                 setHeader("Link", String.format("</%s/%s/history>;rel=\"version-history\"",
                         primaryKey,
                         record.getEntry().get(primaryKey).textValue()));
-    }
-
-    @GET
-    @Path("/entry/{serial}")
-    public SingleResultView findBySerial(@PathParam("serial") int serial) {
-        Optional<DbRecord> recordO = queryDAO.findBySerial(serial);
-        recordO.ifPresent(this::setVersionHistoryLinkHeader);
-        return recordO.map(viewFactory::getSingleResultView)
-                .orElseThrow(NotFoundException::new);
     }
 }
