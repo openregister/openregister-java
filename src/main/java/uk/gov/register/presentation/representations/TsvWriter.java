3157ffc713c9409310f689f44afc181b6fb44ee5
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-09-21 17:33:07 +0100

    Ensure TsvWriter renders all fields
    like previous commit, but for TsvWriter instead of CsvWriter

--- src/main/java/uk/gov/register/presentation/representations/TsvWriter.java
+++ src/main/java/uk/gov/register/presentation/representations/TsvWriter.java
@@ -10,22 +10,23 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import static com.google.common.collect.Lists.newArrayList;
+import java.util.stream.StreamSupport;
 
 @Produces(ExtraMediaType.TEXT_TSV)
 public class TsvWriter extends RepresentationWriter {
     @Override
     protected void writeEntriesTo(OutputStream entityStream, Register register, List<EntryView> entries) throws IOException, WebApplicationException {
-        List<String> fields = newArrayList(entries.get(0).allFields());
+        Iterable<String> fields = register.getFields();
         entityStream.write(("entry\t" + String.join("\t", fields) + "\n").getBytes("utf-8"));
         for (EntryView entry : entries) {
             writeRow(entityStream, fields, entry);
         }
     }
 
-    private void writeRow(OutputStream entityStream, List<String> fields, EntryView entry) throws IOException {
-        String row = fields.stream().map(field -> entry.getField(field).map(FieldValue::value).orElse("")).collect(Collectors.joining("\t", entry.getSerialNumber() + "\t", "\n"));
+    private void writeRow(OutputStream entityStream, Iterable<String> fields, EntryView entry) throws IOException {
+        String row = StreamSupport.stream(fields.spliterator(),false)
+                .map(field -> entry.getField(field).map(FieldValue::value).orElse(""))
+                .collect(Collectors.joining("\t", entry.getSerialNumber() + "\t", "\n"));
         entityStream.write(row.getBytes("utf-8"));
     }
 }
--- src/test/java/uk/gov/register/presentation/representations/TsvWriterTest.java
+++ src/test/java/uk/gov/register/presentation/representations/TsvWriterTest.java
@@ -3,31 +3,28 @@ package uk.gov.register.presentation.representations;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import org.junit.Test;
-import uk.gov.register.presentation.FieldValue;
 import uk.gov.register.presentation.EntryView;
 import uk.gov.register.presentation.StringValue;
 import uk.gov.register.presentation.config.Register;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.Map;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class TsvWriterTest {
+
+    private final TsvWriter writer = new TsvWriter();
+
     @Test
     public void tsv_entriesAreGenerated() throws IOException {
-        TsvWriter writer = new TsvWriter();
-
-
-        Map<String, FieldValue> entryMap = ImmutableMap.of(
+        EntryView entry = new EntryView(52, "hash1", "registerName", ImmutableMap.of(
                 "key1", new StringValue("value1"),
                 "key2", new StringValue("value2"),
                 "key3", new StringValue("val\"ue3"),
                 "key4", new StringValue("value4")
-        );
-        EntryView entry = new EntryView(52, "hash1", "registerName", entryMap);
+        ));
 
         TestOutputStream entityStream = new TestOutputStream();
 
@@ -36,4 +33,16 @@ public class TsvWriterTest {
         assertThat(entityStream.contents, equalTo("entry\tkey1\tkey2\tkey3\tkey4\n52\tvalue1\tvalue2\tval\"ue3\tvalue4\n"));
     }
 
+    @Test
+    public void tsvWriter_rendersAllFieldsEvenIfSomeAreMissing() throws Exception {
+        EntryView entry = new EntryView(52, "hash1", "registerName", ImmutableMap.of(
+                "key1", new StringValue("value1")
+        ));
+
+        TestOutputStream entityStream = new TestOutputStream();
+
+        writer.writeEntriesTo(entityStream, new Register("registerName", ImmutableSet.of("key1", "key2", "key3", "key4"), "", null, ""), Collections.singletonList(entry));
+
+        assertThat(entityStream.contents, equalTo("entry\tkey1\tkey2\tkey3\tkey4\n52\tvalue1\t\t\t\n"));
+    }
 }
