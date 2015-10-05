3cd4ab51c37f7f8c5241b9b024871ae22f6e5f19
omsharma <foromsharma@gmail.com>
2015-10-05 10:26:20 +0100

    Removed unused DAO methods

--- src/main/java/uk/gov/indexer/dao/CurrentKeysUpdateDAO.java
+++ src/main/java/uk/gov/indexer/dao/CurrentKeysUpdateDAO.java
@@ -11,14 +11,8 @@ interface CurrentKeysUpdateDAO extends DBConnectionDAO {
     @SqlUpdate("CREATE TABLE IF NOT EXISTS " + CURRENT_KEYS_TABLE + " (KEY VARCHAR PRIMARY KEY, SERIAL_NUMBER INTEGER UNIQUE)")
     void ensureCurrentKeysTableExists();
 
-    @SqlUpdate("INSERT INTO " + CURRENT_KEYS_TABLE + "(SERIAL_NUMBER, KEY) VALUES(:serial_number, :key)")
-    int writeCurrentKey(@Bind("serial_number") int serial_number, @Bind("key") String key);
-
     @SqlUpdate("UPDATE " + CURRENT_KEYS_TABLE + " SET SERIAL_NUMBER=:serial_number WHERE KEY=:key")
     int updateSerialNumber(@Bind("serial_number") int serial_number, @Bind("key") String key);
-
-    @SqlQuery("SELECT 1 FROM " + CURRENT_KEYS_TABLE + " WHERE KEY=:key")
-    boolean doesRecordExistWithKey(@Bind("key") String key);
 
     @SqlQuery("SELECT KEY FROM " + CURRENT_KEYS_TABLE + " WHERE KEY IN (:keys)")
     List<String> getExistingKeys(@Bind("keys") String keys);
--- src/main/java/uk/gov/indexer/dao/IndexedEntriesUpdateDAO.java
+++ src/main/java/uk/gov/indexer/dao/IndexedEntriesUpdateDAO.java
@@ -1,7 +1,9 @@
 package uk.gov.indexer.dao;
 
-import org.postgresql.util.PGobject;
-import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.BindBean;
+import org.skife.jdbi.v2.sqlobject.SqlBatch;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 
 import java.util.Iterator;
 
@@ -10,9 +12,6 @@ interface IndexedEntriesUpdateDAO extends DBConnectionDAO {
 
     @SqlUpdate("CREATE TABLE IF NOT EXISTS " + INDEXED_ENTRIES_TABLE + " (SERIAL_NUMBER INTEGER PRIMARY KEY, ENTRY JSONB)")
     void ensureIndexedEntriesTableExists();
-
-    @SqlUpdate("INSERT INTO " + INDEXED_ENTRIES_TABLE + "(SERIAL_NUMBER, ENTRY) VALUES(:serial_number, :entry)")
-    int write(@Bind("serial_number") int serial_number, @Bind("entry") PGobject entry);
 
     @SqlQuery("SELECT MAX(SERIAL_NUMBER) FROM " + INDEXED_ENTRIES_TABLE)
     int lastReadSerialNumber();
