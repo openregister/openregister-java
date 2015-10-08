f3b3ae2f8ac5fe78e4f2112a91efe56dfacde194
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2015-10-08 16:22:33 +0100

    deploy environment-specific config

--- deploy/scripts/start-service.sh
+++ deploy/scripts/start-service.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-aws s3 cp s3://preview.config/indexer/indexer.properties /srv/indexer --region eu-west-1
+
+set -eu
+
+INSTANCE_ID=$(curl 169.254.169.254/2014-11-05/meta-data/instance-id)
+export AWS_DEFAULT_REGION=eu-west-1
+ENV=$(aws ec2 describe-tags --filters Name=resource-id,Values=$INSTANCE_ID Name=key,Values=Environment --query 'Tags[0].Value' --output text)
+CONFIG_BUCKET=openregister.${ENV}.config
+
+aws s3 cp s3://${CONFIG_BUCKET}/indexer/indexer.properties /srv/indexer --region eu-west-1
 docker run -d --name=indexerApp \
     --volume /srv/indexer:/srv/indexer \
     jstepien/openjdk8 java -jar /srv/indexer/indexer.jar config.file=/srv/indexer/indexer.properties
