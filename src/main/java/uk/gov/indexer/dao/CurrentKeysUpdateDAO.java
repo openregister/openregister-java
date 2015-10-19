6bf0f8b5fe17ef259bfbb4d27bb890f220ad278b
omsharma <foromsharma@gmail.com>
2015-10-19 15:12:12 +0100

    Using @BindIn for in query instead of creating comma separated string.

--- src/main/java/uk/gov/indexer/dao/CurrentKeysUpdateDAO.java
+++ src/main/java/uk/gov/indexer/dao/CurrentKeysUpdateDAO.java
@@ -1,9 +1,12 @@
 package uk.gov.indexer.dao;
 
 import org.skife.jdbi.v2.sqlobject.*;
+import org.skife.jdbi.v2.sqlobject.stringtemplate.UseStringTemplate3StatementLocator;
+import org.skife.jdbi.v2.unstable.BindIn;
 
 import java.util.List;
 
+@UseStringTemplate3StatementLocator
 interface CurrentKeysUpdateDAO extends DBConnectionDAO {
     String CURRENT_KEYS_TABLE = "CURRENT_KEYS";
 
@@ -13,8 +16,8 @@ interface CurrentKeysUpdateDAO extends DBConnectionDAO {
     @SqlUpdate("UPDATE " + CURRENT_KEYS_TABLE + " SET SERIAL_NUMBER=:serial_number WHERE KEY=:key")
     int updateSerialNumber(@Bind("serial_number") int serial_number, @Bind("key") String key);
 
-    @SqlQuery("SELECT KEY FROM " + CURRENT_KEYS_TABLE + " WHERE KEY IN (:keys)")
-    List<String> getExistingKeys(@Bind("keys") String commaSeparatedKeys);
+    @SqlQuery("SELECT KEY FROM " + CURRENT_KEYS_TABLE + " WHERE KEY IN (<keys>)")
+    List<String> getExistingKeys(@BindIn("keys") Iterable<String> keys);
 
     @SqlBatch("insert into " + CURRENT_KEYS_TABLE + "(SERIAL_NUMBER, KEY) values(:serial_number, :key)")
     void insertEntries(@BindBean Iterable<CurrentKey> keys);
