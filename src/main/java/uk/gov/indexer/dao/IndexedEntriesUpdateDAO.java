d9b604ee2ccc94bea8e3ddf63c88bc6886e02ca1
omsharma <foromsharma@gmail.com>
2015-11-04 11:10:37 +0000

    Resolved bug: introduced in https://github.com/openregister/indexer/pull/23. reverted all lowercase changes only keep table column and table names in lowercase.

--- src/main/java/uk/gov/indexer/dao/IndexedEntriesUpdateDAO.java
+++ src/main/java/uk/gov/indexer/dao/IndexedEntriesUpdateDAO.java
@@ -9,23 +9,23 @@ public interface IndexedEntriesUpdateDAO extends DBConnectionDAO {
     String INDEXED_ENTRIES_TABLE = "ordered_entry_index";
     String INDEXED_ENTRIES_INDEX = INDEXED_ENTRIES_TABLE + "_gin";
 
-    @SqlUpdate("create table if not exists " + INDEXED_ENTRIES_TABLE + " (serial_number integer primary key, entry jsonb)")
+    @SqlUpdate("CREATE TABLE IF NOT EXISTS " + INDEXED_ENTRIES_TABLE + " (serial_number INTEGER PRIMARY KEY, entry JSONB)")
     void ensureIndexedEntriesTableExists();
 
 
-    @SqlUpdate("create index " + INDEXED_ENTRIES_INDEX + " on " + INDEXED_ENTRIES_TABLE + " using gin(entry jsonb_path_ops)")
+    @SqlUpdate("CREATE INDEX " + INDEXED_ENTRIES_INDEX + " ON " + INDEXED_ENTRIES_TABLE + " USING gin(entry jsonb_path_ops)")
     void createIndexedEntriesIndex();
 
-    @SqlQuery("select 1 from pg_indexes where indexname='" + INDEXED_ENTRIES_INDEX + "'")
+    @SqlQuery("SELECT 1 FROM pg_indexes WHERE indexname='" + INDEXED_ENTRIES_INDEX + "'")
     boolean indexedEntriesIndexExists();
 
-    @SqlQuery("select max(serial_number) from " + INDEXED_ENTRIES_TABLE)
+    @SqlQuery("SELECT MAX(serial_number) FROM " + INDEXED_ENTRIES_TABLE)
     int lastReadSerialNumber();
 
-    @SqlBatch("insert into " + INDEXED_ENTRIES_TABLE + "(serial_number, entry) values(:serial_number, :dbentry)")
+    @SqlBatch("INSERT INTO " + INDEXED_ENTRIES_TABLE + "(serial_number, entry) VALUES(:serial_number, :dbEntry)")
     void writeBatch(@BindBean Iterable<OrderedEntryIndex> orderedIndexEntry);
 
     @RegisterMapper(OrderedEntryIndexMapper.class)
-    @SqlQuery("select * from " + INDEXED_ENTRIES_TABLE + " where serial_number > :serial_number order by serial_number limit 5000")
+    @SqlQuery("SELECT * FROM " + INDEXED_ENTRIES_TABLE + " WHERE serial_number > :serial_number ORDER BY serial_number LIMIT 5000")
     List<OrderedEntryIndex> fetchEntriesAfter(@Bind("serial_number") int watermark);
 }
