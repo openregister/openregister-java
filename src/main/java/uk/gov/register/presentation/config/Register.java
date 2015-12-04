a1af07e59fff2512f45fa1ce17fa60cbf28700ba
omsharma <foromsharma@gmail.com>
2015-12-04 10:28:40 +0000

    Refreshed fields.yaml and registers.yaml with the latest change in data

--- src/main/java/uk/gov/register/presentation/config/Register.java
+++ src/main/java/uk/gov/register/presentation/config/Register.java
@@ -14,6 +14,7 @@ import static com.google.common.base.Predicates.not;
 
 public class Register {
     final String registerName;
+    final String phase;
     final Set<String> fields;
     final Optional<String> copyright;
     final String registry;
@@ -24,8 +25,10 @@ public class Register {
                     @JsonProperty("fields") Set<String> fields,
                     @JsonProperty("copyright") String copyright,
                     @JsonProperty("registry") String registry,
-                    @JsonProperty("text") String text) {
+                    @JsonProperty("text") String text,
+                    @JsonProperty("phase") String phase) {
         this.registerName = registerName;
+        this.phase = phase;
         this.fields = new TreeSet<>(fields); // ensure sorted order
         this.copyright = StringUtils.isNotEmpty(copyright) ? Optional.of(copyright) : Optional.empty();
         this.registry = registry;
