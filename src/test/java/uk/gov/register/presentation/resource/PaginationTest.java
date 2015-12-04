7204f86a384d8844ecf29ed0e99d724f65d18290
omsharma <foromsharma@gmail.com>
2015-12-04 14:30:15 +0000

    rendering page navigation header and restyled page navigation footer

--- src/test/java/uk/gov/register/presentation/resource/PaginationTest.java
+++ src/test/java/uk/gov/register/presentation/resource/PaginationTest.java
@@ -18,67 +18,67 @@ public class PaginationTest {
 
     @Test(expected = BadRequestException.class)
     public void construct_throwsExceptionWhenPageSizeIsZero() {
-        new Pagination("/entries", Optional.of(1l), Optional.of(0l), 10);
+        new Pagination("/entries", Optional.of(1L), Optional.of(0L), 10);
     }
 
     @Test(expected = BadRequestException.class)
     public void construct_throwsExceptionWhenPageSizeIsNegativeNumber() {
-        new Pagination("/entries", Optional.of(1l), Optional.of(-1l), 10);
+        new Pagination("/entries", Optional.of(1L), Optional.of(-1L), 10);
     }
 
     @Test(expected = BadRequestException.class)
     public void construct_throwsExceptionWhenPageIndexIsZero() {
-        new Pagination("/entries", Optional.of(0l), Optional.of(1l), 10);
+        new Pagination("/entries", Optional.of(0L), Optional.of(1L), 10);
     }
 
     @Test(expected = BadRequestException.class)
     public void construct_throwsExceptionWhenPageIndexIsNegativeNumber() {
-        new Pagination("/entries", Optional.of(-1l), Optional.of(1l), 10);
+        new Pagination("/entries", Optional.of(-1L), Optional.of(1L), 10);
     }
 
     @Test(expected = NotFoundException.class)
     public void construct_throwsNotFoundException_whenNoMoreEntriesForGivenPageSizeAndPageIndexValues() {
-        new Pagination("/entries", Optional.of(2l), Optional.of(10l), 10);
+        new Pagination("/entries", Optional.of(2L), Optional.of(10L), 10);
     }
 
     @Test
     public void offset_returnsTheNumberWhichOffsetsTheTotalEntriesBasedOnPageSize() {
-        assertThat(new Pagination("/entries", Optional.of(1l), Optional.of(10l), 100).offset(), equalTo(0l));
-        assertThat(new Pagination("/entries", Optional.of(2l), Optional.of(10l), 100).offset(), equalTo(10l));
+        assertThat(new Pagination("/entries", Optional.of(1L), Optional.of(10L), 100).offset(), equalTo(0L));
+        assertThat(new Pagination("/entries", Optional.of(2L), Optional.of(10L), 100).offset(), equalTo(10L));
     }
 
     @Test
     public void hasNextPage_returnsTrueOnlyWhenThereAreMoreEntriesAvailable() {
-        assertFalse(new Pagination("/entries", Optional.of(1l), Optional.of(10l), 10).hasNextPage());
-        assertFalse(new Pagination("/entries", Optional.of(2l), Optional.of(10l), 20).hasNextPage());
+        assertFalse(new Pagination("/entries", Optional.of(1L), Optional.of(10L), 10).hasNextPage());
+        assertFalse(new Pagination("/entries", Optional.of(2L), Optional.of(10L), 20).hasNextPage());
 
-        assertTrue(new Pagination("/entries", Optional.of(1l), Optional.of(10l), 11).hasNextPage());
+        assertTrue(new Pagination("/entries", Optional.of(1L), Optional.of(10L), 11).hasNextPage());
 
-        assertTrue(new Pagination("/entries", Optional.of(2l), Optional.of(10l), 21).hasNextPage());
+        assertTrue(new Pagination("/entries", Optional.of(2L), Optional.of(10L), 21).hasNextPage());
     }
 
     @Test
     public void hasPreviousPage_returnsTrueOnlyWhenPageIndexIsMoreThanOne() {
-        assertFalse(new Pagination("/entries", Optional.of(1l), Optional.of(10l), 10).hasPreviousPage());
-        assertFalse(new Pagination("/entries", Optional.of(1l), Optional.of(10l), 11).hasPreviousPage());
+        assertFalse(new Pagination("/entries", Optional.of(1L), Optional.of(10L), 10).hasPreviousPage());
+        assertFalse(new Pagination("/entries", Optional.of(1L), Optional.of(10L), 11).hasPreviousPage());
 
-        assertTrue(new Pagination("/entries", Optional.of(2l), Optional.of(10l), 11).hasPreviousPage());
+        assertTrue(new Pagination("/entries", Optional.of(2L), Optional.of(10L), 11).hasPreviousPage());
     }
 
     @Test
     public void getTotalPages_returnsTotalNumberOfPages() {
         Pagination pagination = new Pagination(
                 "/entries",
-                Optional.of(1l),
-                Optional.of(10l),
+                Optional.of(1L),
+                Optional.of(10L),
                 10);
         assertThat(pagination.getTotalPages(), equalTo(1L));
 
 
         pagination = new Pagination(
                 "/entries",
-                Optional.of(1l),
-                Optional.of(10l),
+                Optional.of(1L),
+                Optional.of(10L),
                 11);
         assertThat(pagination.getTotalPages(), equalTo(2L));
 
@@ -86,13 +86,28 @@ public class PaginationTest {
 
     @Test
     public void getNextPageLink_returnsTheLinkForNextPage() {
-        Pagination pagination = new Pagination("/entries", Optional.of(1l), Optional.of(10l), 11);
+        Pagination pagination = new Pagination("/entries", Optional.of(1L), Optional.of(10L), 11);
         assertThat(pagination.getNextPageLink(), equalTo("/entries?page-index=2&page-size=10"));
     }
 
     @Test
     public void getPreviousPageLink_returnsTheLinkForPreviousPage() {
-        Pagination pagination = new Pagination("/entries", Optional.of(2l), Optional.of(10l), 11);
+        Pagination pagination = new Pagination("/entries", Optional.of(2L), Optional.of(10L), 11);
         assertThat(pagination.getPreviousPageLink(), equalTo("/entries?page-index=1&page-size=10"));
     }
+
+    @Test
+    public void getFirstEntryNumberOnThisPage_returnsTheNumberOfFirstEntryOnPage() {
+        Pagination pagination = new Pagination("/entries", Optional.of(2L), Optional.of(10L), 11);
+        assertThat(pagination.getFirstEntryNumberOnThisPage(), equalTo(11L));
+    }
+
+    @Test
+    public void getLastEntryNumberOnThisPage_returnsTheNumberOfLastEntryOnPage() {
+        Pagination pagination = new Pagination("/entries", Optional.of(2L), Optional.of(10L), 12);
+        assertThat(pagination.getLastEntryNumberOnThisPage(), equalTo(12L));
+    }
+
+
+
 }
