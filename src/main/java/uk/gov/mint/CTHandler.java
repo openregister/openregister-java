e78e8fa5ce376143f0c4107e61701408ca837bf9
Andrew Gorton <a.gorton@kainos.com>
2015-12-23 12:57:18 +0000

    Added functional tests for CT server using WireMock.

--- src/main/java/uk/gov/mint/CTHandler.java
+++ src/main/java/uk/gov/mint/CTHandler.java
@@ -3,6 +3,8 @@ package uk.gov.mint;
 import com.fasterxml.jackson.databind.JsonNode;
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.setup.Environment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.gov.MintConfiguration;
 
 import javax.ws.rs.client.Client;
@@ -15,6 +17,8 @@ import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 public class CTHandler implements Handler {
+    private final Logger LOGGER = LoggerFactory.getLogger(CTHandler.class);
+
     private final String register;
     private final String ctserver;
     private final Client client;
@@ -22,6 +26,8 @@ public class CTHandler implements Handler {
     private final CanonicalJsonMapper canonicalJsonMapper;
 
     public CTHandler(MintConfiguration configuration, Environment environment, final String appName, EntryValidator entryValidator) {
+        LOGGER.info("CTHandler created");
+
         this.register = configuration.getRegister();
         this.ctserver = configuration.getCTServer();
         this.entryValidator = entryValidator;
@@ -38,6 +44,8 @@ public class CTHandler implements Handler {
     }
 
     private void processEntries(String[] entries) {
+        LOGGER.info("Sending entries to CT server");
+
         WebTarget wt = client.target(ctserver);
 
         Arrays.stream(entries).forEach(e -> {
--- src/test/java/uk/gov/functional/FunctionalTest.java
+++ src/test/java/uk/gov/functional/FunctionalTest.java
@@ -1,6 +1,7 @@
 package uk.gov.functional;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.dropwizard.testing.ConfigOverride;
 import io.dropwizard.testing.ResourceHelpers;
 import io.dropwizard.testing.junit.DropwizardAppRule;
@@ -21,13 +22,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.sql.*;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
 
-
 public class FunctionalTest {
     private static String postgresConnectionString = "jdbc:postgresql://localhost:5432/ft_mint";
+    private static String ctserver = "http://localhost:8089/add-json";
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(8089);
 
     @Rule
     public TestRule ruleChain = RuleChain.
@@ -37,7 +42,8 @@ public class FunctionalTest {
             around(
                     new DropwizardAppRule<>(MintApplication.class,
                             ResourceHelpers.resourceFilePath("test-config.yaml"),
-                            ConfigOverride.config("database.url", postgresConnectionString))
+                            ConfigOverride.config("database.url", postgresConnectionString),
+                            ConfigOverride.config("ctserver", ctserver))
             );
 
 
@@ -45,10 +51,15 @@ public class FunctionalTest {
 
     @Test
     public void checkMessageIsConsumedAndStoredInDatabase() throws Exception {
+        stubFor(post(urlEqualTo("/add-json"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                ));
+
         CanonicalJsonMapper canonicalJsonMapper = new CanonicalJsonMapper();
 
-        send("{\"register\":\"ft_mint_test\",\"text\":\"SomeText\"}");
-
+        Response r =  send("{\"register\":\"ft_mint_test\",\"text\":\"SomeText\"}");
+        assertThat(r.getStatus(), equalTo(200));
 
         JsonNode storedEntry = canonicalJsonMapper.readFromBytes(tableRecord());
 
@@ -85,6 +96,18 @@ public class FunctionalTest {
         Response response = send(entry);
         assertThat(response.getStatus(), equalTo(400));
         assertThat(response.readEntity(String.class), equalTo("Field 'fields' has cardinality 'n' so the value must be an array of 'string'. Error entry: '" + entry + "'"));
+    }
+
+    @Test
+    public void checkThatErrorsFromCTServerArePropogatedBack() throws Exception {
+        stubFor(post(urlEqualTo("/add-json"))
+                .willReturn(aResponse()
+                        .withStatus(400)
+                        .withBody("Unable to parse JSON")
+                ));
+
+        Response r =  send("{\"register\":\"ft_mint_test\",\"text\":\"SomeText\"}");
+        assertThat(r.getStatus(), equalTo(400));
     }
 
     private Response send(String... payload) {
