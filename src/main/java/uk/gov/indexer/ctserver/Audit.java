e90bb6a01444f7ca6e072548420f4f5b114bf184
Andrew Gorton <a.gorton@kainos.com>
2016-01-11 16:13:02 +0000

    Missing file added.

---
+++ src/main/java/uk/gov/indexer/ctserver/Audit.java
@@ -0,0 +1,24 @@
+package uk.gov.indexer.ctserver;
+
+import java.util.List;
+
+public class Audit {
+    private int leaf_index;
+    private List<String> audit_path;
+
+    public int getLeaf_index() {
+        return leaf_index;
+    }
+
+    public void setLeaf_index(int leaf_index) {
+        this.leaf_index = leaf_index;
+    }
+
+    public List<String> getAudit_path() {
+        return audit_path;
+    }
+
+    public void setAudit_path(List<String> audit_path) {
+        this.audit_path = audit_path;
+    }
+}
