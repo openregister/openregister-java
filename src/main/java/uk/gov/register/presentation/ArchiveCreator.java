93ae5f88054af27ff24990f4c4db2a65369d147d
Andrew Gorton <a.gorton@kainos.com>
2016-01-28 08:30:39 +0000

    Changed item filenames to be the item hash. Prevent duplicate items (previously, underlying library throws an exception). Started adding entries to include item hash and entry number.

--- src/main/java/uk/gov/register/presentation/ArchiveCreator.java
+++ src/main/java/uk/gov/register/presentation/ArchiveCreator.java
@@ -8,7 +8,9 @@ import uk.gov.register.proofs.ct.SignedTreeHead;
 import javax.ws.rs.core.StreamingOutput;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -30,12 +32,34 @@ public class ArchiveCreator {
             om.writeValue(zos, proofs);
             zos.closeEntry();
 
+            // Add each item
+            // Prevent duplicate files being added
+            Set<String> itemHashesAdded = new HashSet<>();
+            entries.forEach(singleEntry -> {
+                if (!itemHashesAdded.contains(singleEntry.getContent().getHash())) {
+                    JsonNode entryData = singleEntry.getContent().getContent();
+                    ZipEntry singleZipEntry = new ZipEntry(String.format("item/%s.json", singleEntry.getContent().getHash()));
+                    try {
+                        zos.putNextEntry(singleZipEntry);
+                        zos.write(entryData.toString().getBytes(StandardCharsets.UTF_8));
+                        zos.closeEntry();
+                        itemHashesAdded.add(singleEntry.getContent().getHash());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            });
+
+            // Add each entry record
+            // Keyed by entry, should contain entryNumber, itemHash, timestamp
             entries.forEach(singleEntry -> {
                 JsonNode entryData = singleEntry.getContent().getContent();
-                ZipEntry singleZipEntry = new ZipEntry(String.format("item/%s.json", singleEntry.getSerialNumber()));
+                ZipEntry singleZipEntry = new ZipEntry(String.format("entry/%d.json", singleEntry.getSerialNumber()));
                 try {
                     zos.putNextEntry(singleZipEntry);
-                    zos.write(entryData.toString().getBytes(StandardCharsets.UTF_8));
+
+                    ArchiveEntryData aed = new ArchiveEntryData(singleEntry.getSerialNumber(), singleEntry.getContent().getHash(), 0);
+                    om.writeValue(zos, aed);
                     zos.closeEntry();
                 } catch (IOException e) {
                     throw new RuntimeException(e);
@@ -44,5 +68,17 @@ public class ArchiveCreator {
             zos.flush();
             zos.close();
         };
+    }
+
+    public static class ArchiveEntryData {
+        public int entryNumber;
+        public String itemHash;
+        public long timestamp;
+
+        public ArchiveEntryData(int entryNumber, String itemHash, long timestamp) {
+            this.entryNumber = entryNumber;
+            this.itemHash = itemHash;
+            this.timestamp = timestamp;
+        }
     }
 }
