e98605f2bd81499aeeb0b1da1f63a7ea98cdc667
Om Sharma <foromsharma@gmail.com>
2016-03-08 12:06:38 +0000

    running psql queries quietly

--- create_schema.sh
+++ create_schema.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-db_name='ft_presentation'
+db_name=$1
 
 createdb $db_name
 
-psql $db_name -U postgres -c "CREATE TABLE IF NOT EXISTS current_keys(key VARCHAR PRIMARY KEY, serial_number INTEGER UNIQUE)"
-psql $db_name -U postgres -c "CREATE TABLE IF NOT EXISTS total_records(count INTEGER)"
+psql $db_name -U postgres -q -S -c "
 
-psql $db_name -U postgres -c "insert into total_records (count) select 0 where not exists (select count from total_records)"
+CREATE TABLE IF NOT EXISTS current_keys(key VARCHAR PRIMARY KEY, serial_number INTEGER UNIQUE);
 
-psql $db_name -U postgres -c "CREATE TABLE IF NOT EXISTS ordered_entry_index(serial_number INTEGER PRIMARY KEY, entry JSONB)"
-psql $db_name -U postgres -c "ALTER TABLE ordered_entry_index drop column if exists leaf_input"
+CREATE TABLE IF NOT EXISTS total_records(count INTEGER);
 
-psql $db_name -U postgres -c "CREATE TABLE IF NOT EXISTS total_entries(count INTEGER, last_updated TIMESTAMP WITHOUT TIME ZONE DEFAULT now())"
-psql $db_name -U postgres -c "CREATE TABLE sth (tree_size INTEGER, timestamp BIGINT, tree_head_signature VARCHAR, sha256_root_hash VARCHAR)"
+insert into total_records (count) select 0 where not exists (select count from total_records);
 
-psql $db_name -U postgres -c "insert into total_entries (count) select 0 where not exists (select count from total_entries)"
+CREATE TABLE IF NOT EXISTS ordered_entry_index(serial_number INTEGER PRIMARY KEY, entry JSONB);
+
+ALTER TABLE ordered_entry_index drop column if exists leaf_input;
+
+CREATE TABLE IF NOT EXISTS total_entries(count INTEGER, last_updated TIMESTAMP WITHOUT TIME ZONE DEFAULT now());
+
+insert into total_entries (count) select 0 where not exists (select count from total_entries);
+
+"
--- drop_schema.sh
+++ drop_schema.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-db_name='ft_presentation'
+db_name=$1
 
-psql $db_name -U postgres -c "DROP TABLE IF EXISTS current_keys"
-psql $db_name -U postgres -c "DROP TABLE IF EXISTS total_records"
-psql $db_name -U postgres -c "DROP TABLE IF EXISTS ordered_entry_index"
-psql $db_name -U postgres -c "DROP TABLE IF EXISTS total_entries"
+psql $db_name -U postgres -q -S -c "
+
+DROP TABLE IF EXISTS current_keys;
+
+DROP TABLE IF EXISTS total_records;
+
+DROP TABLE IF EXISTS ordered_entry_index;
+
+DROP TABLE IF EXISTS total_entries;
+
+DROP TABLE IF EXISTS sth; -- TODO: no longer used, should be deleted after a while
+
+"
