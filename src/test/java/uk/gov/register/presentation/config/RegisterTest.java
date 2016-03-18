0e299ec7943762d060078042d219646416804aa9
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-03-18 13:30:01 +0000

    fix tests
    some tests (implicitly or explicitly) expected the old ordering
    behaviour.

--- src/test/java/uk/gov/register/presentation/config/RegisterTest.java
+++ src/test/java/uk/gov/register/presentation/config/RegisterTest.java
@@ -1,6 +1,6 @@
 package uk.gov.register.presentation.config;
 
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableList;
 import org.hamcrest.collection.IsIterableContainingInOrder;
 import org.junit.Test;
 
@@ -8,20 +8,20 @@ import static org.junit.Assert.assertThat;
 
 public class RegisterTest {
     @Test
-    public void getFields_returnsFieldsInSortedOrder() throws Exception {
-        Register register = new Register("address", ImmutableSet.of("address", "street", "postcode", "area", "property"), "", "", "", "alpha");
+    public void getFields_returnsFieldsInConfiguredOrder() throws Exception {
+        Register register = new Register("address", ImmutableList.of("address", "street", "postcode", "area", "property"), "", "", "", "alpha");
 
         Iterable<String> fields = register.getFields();
 
-        assertThat(fields, IsIterableContainingInOrder.contains("address", "area", "postcode", "property", "street"));
+        assertThat(fields, IsIterableContainingInOrder.contains("address", "street", "postcode", "area", "property"));
     }
 
     @Test
-    public void getNonPrimaryFields_returnsFieldsOtherThanPrimaryInSortedOrder() throws Exception {
-        Register register = new Register("company", ImmutableSet.of("address", "company", "secretary", "company-status", "company-accounts-category"), "", "", "", "alpha");
+    public void getNonPrimaryFields_returnsFieldsOtherThanPrimaryInConfiguredOrder() throws Exception {
+        Register register = new Register("company", ImmutableList.of("address", "company", "secretary", "company-status", "company-accounts-category"), "", "", "", "alpha");
 
         Iterable<String> fields = register.getNonPrimaryFields();
 
-        assertThat(fields, IsIterableContainingInOrder.contains("address", "company-accounts-category", "company-status", "secretary"));
+        assertThat(fields, IsIterableContainingInOrder.contains("address","secretary", "company-status", "company-accounts-category"));
     }
 }
