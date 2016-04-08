6123c050899d7e5eb30462196c273200c88200a9
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-04-08 15:10:45 +0100

    add test for item resource json representation

--- src/test/java/uk/gov/register/presentation/functional/RepresentationsTest.java
+++ src/test/java/uk/gov/register/presentation/functional/RepresentationsTest.java
@@ -17,15 +17,18 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
 @RunWith(Parameterized.class)
 public class RepresentationsTest extends FunctionalTestBase {
     public static final String REGISTER_NAME = "register";
     private final String extension;
     private final String expectedContentType;
-    private final String expectedSingleEntry;
-    private final String expectedListOfEntries;
+    private final String expectedItemValue;
+    private final String expectedEntryValue;
+    private final String expectedRecordsValue;
 
     @Before
     public void publishTestMessages() {
@@ -43,35 +46,47 @@ public class RepresentationsTest extends FunctionalTestBase {
 //                {"csv", "text/csv;charset=UTF-8", fixture("fixtures/single.csv"), fixture("fixtures/list.csv")},
 //                {"tsv", "text/tab-separated-values;charset=UTF-8", fixture("fixtures/single.tsv"), fixture("fixtures/list.tsv")},
 //                {"ttl", "text/turtle;charset=UTF-8", fixture("fixtures/single.ttl"), fixture("fixtures/list.ttl")},
-                {"json", "application/json", fixture("fixtures/single.json"), fixture("fixtures/list.json")},
-                {"yaml", "text/yaml;charset=UTF-8", fixture("fixtures/single.yaml"), fixture("fixtures/list.yaml")}
+                {"json", "application/json", fixture("fixtures/item.json"), fixture("fixtures/entry.json"), fixture("fixtures/list.json")},
+                {"yaml", "text/yaml;charset=UTF-8", null, fixture("fixtures/entry.yaml"), fixture("fixtures/list.yaml")}
         });
     }
 
-    public RepresentationsTest(String extension, String expectedContentType, String expectedSingleEntry, String expectedListOfEntries) {
+    public RepresentationsTest(String extension, String expectedContentType, String expectedItemValue, String expectedEntryValue, String expectedRecordsValue) {
         this.extension = extension;
         this.expectedContentType = expectedContentType;
-        this.expectedSingleEntry = expectedSingleEntry;
-        this.expectedListOfEntries = expectedListOfEntries;
+        this.expectedItemValue = expectedItemValue;
+        this.expectedEntryValue = expectedEntryValue;
+        this.expectedRecordsValue = expectedRecordsValue;
     }
 
     @Test
-    public void representationIsSupportedForSingleEntryView() {
+    public void representationIsSupportedForEntryResource() {
         Response response = getRequest(REGISTER_NAME, "/entry/1." + extension);
 
         assertThat(response.getStatus(), equalTo(200));
         assertThat(response.getHeaderString("Content-Type"), equalTo(expectedContentType));
-        assertThat(response.readEntity(String.class), equalTo(expectedSingleEntry));
+        assertThat(response.readEntity(String.class), equalTo(expectedEntryValue));
+    }
+
+    @Test
+    public void representationIsSupportedForItemResource() {
+        assumeThat(expectedItemValue, notNullValue());
+
+        Response response = getRequest(REGISTER_NAME, "/item/sha-256:877d8bd1ab71dc6e48f64b4ca83c6d7bf645a1eb56b34d50fa8a833e1101eb18." + extension);
+
+        assertThat(response.getStatus(), equalTo(200));
+        assertThat(response.getHeaderString("Content-Type"), equalTo(expectedContentType));
+        assertThat(response.readEntity(String.class), equalTo(expectedItemValue));
     }
 
     @Test
     @Ignore("/records doesn't yet support the new yaml format")
-    public void representationIsSupportedForListEntryView() {
+    public void representationIsSupportedForRecordsResource() {
         Response response = getRequest(REGISTER_NAME, "/records." + extension);
 
         assertThat(response.getStatus(), equalTo(200));
         assertThat(response.getHeaderString("Content-Type"), equalTo(expectedContentType));
-        assertThat(response.readEntity(String.class), equalTo(expectedListOfEntries));
+        assertThat(response.readEntity(String.class), equalTo(expectedRecordsValue));
     }
 
     private static String fixture(String resourceName) {
