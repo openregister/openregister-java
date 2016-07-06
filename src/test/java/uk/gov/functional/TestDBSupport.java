c06a89ce60ff29bacba021f0faa75a2c58910d40
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-07-06 14:20:15 +0100

    add FIXME to remind myself to fix this

--- src/test/java/uk/gov/functional/TestDBSupport.java
+++ src/test/java/uk/gov/functional/TestDBSupport.java
@@ -6,6 +6,7 @@ import uk.gov.functional.db.TestEntryDAO;
 import uk.gov.functional.db.TestItemDAO;
 
 public class TestDBSupport {
+    // FIXME: this shouldn't be ft_mint any more
     public static String postgresConnectionString = "jdbc:postgresql://localhost:5432/ft_mint?user=postgres";
     public static final TestEntryDAO testEntryDAO;
     public static final TestItemDAO testItemDAO;
