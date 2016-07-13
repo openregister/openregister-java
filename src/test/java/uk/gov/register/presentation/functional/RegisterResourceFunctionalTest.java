45035c1a66b832c176a1efd4b3b9acf75b8cc916
Chris Duraj <christopherduraj@gmail.com>
2016-07-13 15:46:08 +0100

    Fix Register resource functional tests

--- src/test/java/uk/gov/register/presentation/functional/RegisterResourceFunctionalTest.java
+++ src/test/java/uk/gov/register/presentation/functional/RegisterResourceFunctionalTest.java
@@ -5,39 +5,40 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import io.dropwizard.jackson.Jackson;
-import org.junit.Ignore;
 import org.junit.Test;
 import uk.gov.register.presentation.config.ResourceYamlFileReader;
 
 import javax.ws.rs.core.Response;
 import java.io.IOException;
+import java.time.Instant;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAccessor;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.hasEntry;
 import static uk.gov.register.presentation.functional.TestEntry.anEntry;
 
 
-@Ignore("this test requires the app to be deployed as a register register rather than an address register")
 public class RegisterResourceFunctionalTest extends FunctionalTestBase {
 
+    private final Map<?,?> expectedAddressRegisterMap = getAddressRegisterMap();
+
     @Test
-    public void registerJsonShouldContainEntryViewRegisterRegister() throws Throwable {
+    public void registerJsonShouldContainEntryViewAddressRegister() throws Throwable {
         populateRegisterRegisterEntries();
         Response addressRecordInRegisterRegisterResponse = getRequest("register", "/record/address.json");
         assertThat(addressRecordInRegisterRegisterResponse.getStatus(), equalTo(200));
-        Map addressRecordMapInRegisterRegister = addressRecordInRegisterRegisterResponse.readEntity(Map.class);
-        verifyStringIsAnISODate(addressRecordMapInRegisterRegister.remove("entry-timestamp").toString());
+        Map<?,?> addressRecordMapInRegisterRegister = addressRecordInRegisterRegisterResponse.readEntity(Map.class);
+        verifyStringIsAnISODate(addressRecordMapInRegisterRegister.get("entry-timestamp").toString());
 
-        // FIXME this method was removed. you could invoke WipeDatabaseRule at this point? /shrug
-        // resetSchema();
+        assertAddressRegisterMapIsEqualTo(addressRecordMapInRegisterRegister);
+    }
 
+    @Test
+    public void registerJsonShouldContainEntryViewRegisterRegister() throws Throwable {
         populateAddressRegisterEntries();
 
         Response registerResourceFromAddressRegisterResponse = getRequest("address", "/register.json");
@@ -50,10 +51,10 @@ public class RegisterResourceFunctionalTest extends FunctionalTestBase {
         assertThat(registerResourceMapFromAddressRegister.get("total-items"), equalTo(5));
         verifyStringIsAnISODate(registerResourceMapFromAddressRegister.get("last-updated").toString());
 
-        Map registerRecordMapFromAddressRegister = (Map)registerResourceMapFromAddressRegister.get("register-record");
-        verifyStringIsAnISODate(registerRecordMapFromAddressRegister.remove("entry-timestamp").toString());
+        Map<?,?> registerRecordMapFromAddressRegister = (Map)registerResourceMapFromAddressRegister.get("register-record");
+        verifyStringIsAnISODate(registerRecordMapFromAddressRegister.get("entry-timestamp").toString());
 
-        assertThat(registerRecordMapFromAddressRegister.toString(), equalTo(addressRecordMapInRegisterRegister.toString()));
+        assertAddressRegisterMapIsEqualTo(registerRecordMapFromAddressRegister);
     }
 
     private void populateAddressRegisterEntries() {
@@ -76,12 +77,18 @@ public class RegisterResourceFunctionalTest extends FunctionalTestBase {
 
         List<TestEntry> registerEntries = registers.stream().map(r -> {
             int entryNumber = Integer.parseInt(r.remove("entry-number").toString());
-            r.remove("entry-timestamp");
+            String timestampISO = (String) r.remove("entry-timestamp");
             r.remove("item-hash");
-            return anEntry(entryNumber, writeToString(r));
+            return anEntry(entryNumber, writeToString(r), Instant.parse(timestampISO));
         }).collect(Collectors.toList());
 
         dbSupport.publishEntries("register", registerEntries);
+    }
+
+    private void assertAddressRegisterMapIsEqualTo(Map<?, ?> sutAddressRecordMapInRegisterRegister) {
+        for (Map.Entry entry : expectedAddressRegisterMap.entrySet()) {
+            assertThat(sutAddressRecordMapInRegisterRegister, hasEntry(entry.getKey(), entry.getValue()));
+        }
     }
 
     private String writeToString(Map map) {
@@ -96,5 +103,19 @@ public class RegisterResourceFunctionalTest extends FunctionalTestBase {
         DateTimeFormatter isoFormatter = DateTimeFormatter.ISO_INSTANT;
         TemporalAccessor parsedDate = isoFormatter.parse(lastUpdated);
         assertThat(isoFormatter.format(parsedDate), equalTo(lastUpdated));
+    }
+
+    private HashMap<String, Object> getAddressRegisterMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("entry-number", "1");
+        result.put("item-hash", "sha-256:6a7bcb516b4a465f3340a31ab42a35a9193e512608d9577388da4d99251880a8");
+        result.put("entry-timestamp", "2016-04-21T10:14:21Z");
+        result.put("text", "Postal addresses in the UK");
+        result.put("phase", "alpha");
+        result.put("fields", Arrays.asList("address", "property", "street", "locality", "town", "area", "postcode", "country", "latitude", "longitude"));
+        result.put("register", "address");
+        result.put("registry", "office-for-national-statistics");
+        result.put("copyright", "Contains Ordnance Survey data © Crown copyright & database right 2015\n Contains Royal Mail data © Royal Mail copyright & database right 2015\n");
+        return result;
     }
 }
