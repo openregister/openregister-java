14a3dfb2055d46341edd94955c5fde9e777c88c7
Chris Duraj <christopherduraj@gmail.com>
2016-07-25 12:01:05 +0100

    Simplify calendar creation in EntryMapper

--- src/main/java/uk/gov/register/presentation/dao/EntryMapper.java
+++ src/main/java/uk/gov/register/presentation/dao/EntryMapper.java
@@ -11,9 +11,7 @@ import java.util.TimeZone;
 public class EntryMapper implements ResultSetMapper<Entry> {
     @Override
     public Entry map(int index, ResultSet r, StatementContext ctx) throws SQLException {
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeZone(TimeZone.getTimeZone("UTC"));
-
+        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         return new Entry(r.getString("entry_number"), r.getString("sha256hex"), r.getTimestamp("timestamp", cal).toInstant());
     }
 }
