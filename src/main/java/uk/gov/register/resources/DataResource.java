42cea49be4f81f61c5c28490e1958240695d06e8
Chris Duraj <christopherduraj@gmail.com>
2016-08-01 09:32:42 +0100

    Move '/load' resource from Mint to Register.resources package

--- src/main/java/uk/gov/register/resources/DataResource.java
+++ src/main/java/uk/gov/register/resources/DataResource.java
@@ -1,10 +1,17 @@
 package uk.gov.register.resources;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Throwables;
 import io.dropwizard.views.View;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import uk.gov.mint.ItemValidator;
+import uk.gov.mint.ObjectReconstructor;
 import uk.gov.register.configuration.RegisterNameConfiguration;
 import uk.gov.register.core.Entry;
 import uk.gov.register.core.Item;
 import uk.gov.register.db.EntryQueryDAO;
+import uk.gov.register.db.EntryStore;
 import uk.gov.register.db.ItemQueryDAO;
 import uk.gov.register.db.RecordQueryDAO;
 import uk.gov.register.presentation.ArchiveCreator;
@@ -12,10 +19,11 @@ import uk.gov.register.core.RegisterDetail;
 import uk.gov.register.api.representations.ExtraMediaType;
 import uk.gov.register.views.ViewFactory;
 
+import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -23,19 +31,45 @@ import java.util.Collection;
 
 @Path("/")
 public class DataResource {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     protected final ViewFactory viewFactory;
     private final ItemQueryDAO itemDAO;
-    private RecordQueryDAO recordDAO;
-    private final EntryQueryDAO entryDAO;
+    private final RecordQueryDAO recordDAO;
+    private EntryQueryDAO entryDAO;
     private String registerPrimaryKey;
+    private ObjectReconstructor objectReconstructor;
+    private ItemValidator itemValidator;
+    private EntryStore entryStore;
 
     @Inject
-    public DataResource(ViewFactory viewFactory, RecordQueryDAO recordDAO, EntryQueryDAO entryDAO, ItemQueryDAO itemDAO, RegisterNameConfiguration registerNameConfiguration) {
+    public DataResource(ViewFactory viewFactory, RecordQueryDAO recordDAO, EntryQueryDAO entryDAO, ItemQueryDAO itemDAO, RegisterNameConfiguration registerNameConfiguration, ObjectReconstructor objectReconstructor, ItemValidator itemValidator, EntryStore entryStore) {
         this.viewFactory = viewFactory;
         this.recordDAO = recordDAO;
         this.entryDAO = entryDAO;
         this.itemDAO = itemDAO;
+        this.objectReconstructor = objectReconstructor;
+        this.itemValidator = itemValidator;
+        this.entryStore = entryStore;
         this.registerPrimaryKey = registerNameConfiguration.getRegister();
+    }
+
+    @Context
+    HttpServletRequest httpServletRequest;
+
+    @POST
+    @PermitAll
+    @Path("/load")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void load(String payload) {
+        try {
+            Iterable<JsonNode> objects = objectReconstructor.reconstruct(payload.split("\n"));
+            objects.forEach(singleObject -> itemValidator.validateItem(registerPrimaryKey, singleObject));
+            entryStore.load(registerPrimaryKey, objects);
+        } catch (Throwable t) {
+            logger.error(Throwables.getStackTraceAsString(t));
+            throw t;
+        }
     }
 
     @GET
