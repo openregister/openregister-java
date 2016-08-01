d5c93ef91acd76211c70ff5cfe97791a1f36a78b
Chris Duraj <christopherduraj@gmail.com>
2016-08-01 17:14:53 +0100

    Extract ItemMapper to db.mappers package

--- src/main/java/uk/gov/register/db/ItemQueryDAO.java
+++ src/main/java/uk/gov/register/db/ItemQueryDAO.java
@@ -1,19 +1,12 @@
 package uk.gov.register.db;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.dropwizard.jackson.Jackson;
-import org.skife.jdbi.v2.StatementContext;
 import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 import org.skife.jdbi.v2.sqlobject.customizers.SingleValueResult;
-import org.skife.jdbi.v2.tweak.ResultSetMapper;
 import uk.gov.register.core.Item;
+import uk.gov.register.db.mappers.ItemMapper;
 
-import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -28,22 +21,4 @@ public interface ItemQueryDAO {
     @SqlQuery("select * from item")
     @RegisterMapper(ItemMapper.class)
     Collection<Item> getAllItemsNoPagination();
-
-    class ItemMapper implements ResultSetMapper<Item> {
-
-        private final ObjectMapper objectMapper;
-
-        public ItemMapper() {
-            objectMapper = Jackson.newObjectMapper();
-        }
-
-        @Override
-        public Item map(int index, ResultSet r, StatementContext ctx) throws SQLException {
-            try {
-                return new Item(r.getString("sha256hex"), objectMapper.readValue(r.getString("content"), JsonNode.class));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
---
+++ src/main/java/uk/gov/register/db/mappers/ItemMapper.java
@@ -0,0 +1,33 @@
+package uk.gov.register.db.mappers;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.dropwizard.jackson.Jackson;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+import uk.gov.register.core.Entry;
+import uk.gov.register.core.Item;
+
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class ItemMapper implements ResultSetMapper<Item> {
+
+    private final ObjectMapper objectMapper;
+
+    public ItemMapper() {
+        objectMapper = Jackson.newObjectMapper();
+    }
+
+    @Override
+    public Item map(int index, ResultSet r, StatementContext ctx) throws SQLException {
+        try {
+            return new Item(r.getString("sha256hex"), objectMapper.readValue(r.getString("content"), JsonNode.class));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
