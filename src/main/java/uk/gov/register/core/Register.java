90f29e73a08c9183224081891cbdbd9cb028f5e1
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-09-23 14:17:06 +0100

    organise Register interfaces

--- src/main/java/uk/gov/register/core/Register.java
+++ src/main/java/uk/gov/register/core/Register.java
@@ -7,5 +7,4 @@ public interface Register extends RegisterReadOnly {
     void addItem(Item item); // TODO: what about batches?
     void addEntry(Entry entry); // record handled by this automatically. is it right that user specifies entry number?
     void addItemAndEntry(Item item, Entry entry);
-
 }
--- src/main/java/uk/gov/register/core/RegisterReadOnly.java
+++ src/main/java/uk/gov/register/core/RegisterReadOnly.java
@@ -11,31 +11,24 @@ import java.util.List;
 import java.util.Optional;
 
 public interface RegisterReadOnly {
-    Optional<Entry> getEntry(int entryNumber);
-
     Optional<Item> getItemBySha256(String sha256hex);
-
-    int getTotalEntries();
-
-    Collection<Entry> getEntries(int start, int limit);
-
-    Optional<Record> getRecord(String key); // TODO: is "record" the right word?
-
-    Collection<Entry> getAllEntries();
-
     Collection<Item> getAllItems();
 
+    Optional<Entry> getEntry(int entryNumber);
+    Collection<Entry> getEntries(int start, int limit);
+    Collection<Entry> getAllEntries();
+    int getTotalEntries();
+
+    Optional<Instant> getLastUpdatedTime();
+
+    Optional<Record> getRecord(String key); // TODO: is "record" the right word?
     int getTotalRecords();
     Collection<Entry> allEntriesOfRecord(String key);
     List<Record> getRecords(int limit, int offset);
 
     List<Record> max100RecordsFacetedByKeyValue(String key, String value);
 
-    Optional<Instant> getLastUpdatedTime();
-
     RegisterProof getRegisterProof() throws NoSuchAlgorithmException;
-
     EntryProof getEntryProof(int entryNumber, int totalEntries);
-
     ConsistencyProof getConsistencyProof(int totalEntries1, int totalEntries2);
 }
