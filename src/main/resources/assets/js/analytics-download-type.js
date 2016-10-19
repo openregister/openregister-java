b7061a84bc404af2e2d4c34ebd38dc2d869cbb04
Chris Duraj <christopherduraj@gmail.com>
2016-10-19 15:33:20 +0100

    Add generic data download GA event for /download page

--- src/main/resources/assets/js/analytics-download-type.js
+++ src/main/resources/assets/js/analytics-download-type.js
@@ -3,3 +3,7 @@ var elems = document.getElementsByClassName("js-download");
 setupEventAnalytics(elems, "Data", "download", function(e){
     return e.target.getAttribute("data-download-type") || "unrecognised";
 });
+
+setupEventAnalytics(elems, "Data", "download", function(e){
+    return "data-download";
+});
--- src/main/resources/assets/js/analytics.js
+++ src/main/resources/assets/js/analytics.js
@@ -12,20 +12,20 @@ ga('send', 'pageview');
 
 var setupEventAnalytics = function(elems, category, action, fnLabel){
     for(var i=0; i < elems.length; i++) {
-        elems[i].onclick = function(e){
+        elems[i].addEventListener("click", function(e){
             var targetLabel = fnLabel(e);
             console.log("sending event - category: "+ category + " action: "+ action + " label: "+ targetLabel +" target: "+ e.target);
             ga('send', 'event', category, action, targetLabel);
-        };
+        }, false);
     };
 };
 
 var setupVirtualPageviewAnalytics = function(elems, fnPath){
     for(var i=0; i < elems.length; i++) {
-        elems[i].onclick = function(e){
+        elems[i].addEventListener("click", function(e){
             var targetPath = fnPath(e);
             console.log("sending virtual pageview - path: "+targetPath+" target: "+ e.target);
             ga('send', 'pageview', targetPath);
-        };
+        }, false);
     };
 };
