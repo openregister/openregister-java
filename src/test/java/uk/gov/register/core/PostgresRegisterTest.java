69671696794bdf0f800116b525197511fe67681a
Michaela Benyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2016-11-08 12:44:48 +0000

    Add tests for append-entry in PostgresRegister

--- src/test/java/uk/gov/register/core/PostgresRegisterTest.java
+++ src/test/java/uk/gov/register/core/PostgresRegisterTest.java
@@ -1,12 +1,16 @@
 package uk.gov.register.core;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import uk.gov.register.exceptions.NoSuchFieldException;
+import uk.gov.register.exceptions.NoSuchItemForEntryException;
 import uk.gov.register.service.ItemValidator;
 import uk.gov.register.store.BackingStoreDriver;
 
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import static org.mockito.Mockito.*;
 
@@ -40,5 +44,61 @@ public class PostgresRegisterTest {
         PostgresRegister register = new PostgresRegister(registerData, backingStoreDriver, itemValidator);
         register.max100RecordsFacetedByKeyValue("name", "United Kingdom");
         verify(backingStoreDriver, times(1)).findMax100RecordsByKeyValue("name", "United Kingdom");
+    }
+
+    @Test(expected = NoSuchItemForEntryException.class)
+    public void appendEntryShouldThrowExceptionIfNoCorrespondingItemExists() {
+        Entry entryDangling = new Entry(106, "item-hash-2", Instant.now());
+
+        when(backingStoreDriver.getItemBySha256(anyString())).thenReturn(Optional.empty());
+
+        PostgresRegister register = new PostgresRegister(registerData, backingStoreDriver, itemValidator);
+        register.appendEntry(entryDangling);
+    }
+
+    @Test
+    public void appendEntryShouldNotInsertDanglingEntry() {
+        Item item = new Item("item-hash-1", new ObjectMapper().createObjectNode());
+        Entry entryNotDangling = new Entry(105, "item-hash-1", Instant.now());
+        Entry entryDangling = new Entry(106, "item-hash-2", Instant.now());
+
+        when(backingStoreDriver.getItemBySha256(anyString()))
+                .thenReturn(Optional.of(item))
+                .thenReturn(Optional.empty());
+
+        PostgresRegister register = new PostgresRegister(registerData, backingStoreDriver, itemValidator);
+
+        try {
+            register.appendEntry(entryNotDangling);
+            register.appendEntry(entryDangling);
+        } catch (NoSuchItemForEntryException ex) {
+        }
+
+        verify(backingStoreDriver, times(1)).insertEntry(any());
+    }
+
+    @Test
+    public void appendEntryShouldNotInsertRecordForDanglingEntry() {
+        Item item = new Item("item-hash-1", new ObjectMapper().createObjectNode());
+        Entry entryNotDangling = new Entry(105, "item-hash-1", Instant.now());
+        Entry entryDangling = new Entry(106, "item-hash-2", Instant.now());
+
+        when(backingStoreDriver.getItemBySha256(anyString()))
+                .thenReturn(Optional.of(item))
+                .thenReturn(Optional.empty());
+
+        RegisterMetadata registerMetadata = mock(RegisterMetadata.class);
+        when(registerMetadata.getRegisterName()).thenReturn("country");
+        when(registerData.getRegister()).thenReturn(registerMetadata);
+
+        PostgresRegister register = new PostgresRegister(registerData, backingStoreDriver, itemValidator);
+
+        try {
+            register.appendEntry(entryNotDangling);
+            register.appendEntry(entryDangling);
+        } catch (NoSuchItemForEntryException ex) {
+        }
+
+        verify(backingStoreDriver, times(1)).insertRecord(any(), eq("country"));
     }
 }
