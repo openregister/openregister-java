6f32a351303a637124166154b7ffe092c5afd6d3
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2016-11-11 10:04:55 +0000

    Revert changes to DataUpload and ObjectReconstructor, refactor CanonicalJsonValidator and add an additional test

--- src/main/java/uk/gov/register/resources/DataUpload.java
+++ src/main/java/uk/gov/register/resources/DataUpload.java
@@ -9,7 +9,6 @@ import uk.gov.register.core.Entry;
 import uk.gov.register.core.Item;
 import uk.gov.register.core.Register;
 import uk.gov.register.service.RegisterService;
-import uk.gov.register.util.CanonicalJsonValidator;
 import uk.gov.register.util.ObjectReconstructor;
 import uk.gov.register.views.ViewFactory;
 
@@ -20,9 +19,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Function;
 
 @Path("/")
 public class DataUpload {
@@ -31,14 +28,12 @@ public class DataUpload {
     protected final ViewFactory viewFactory;
     private RegisterService registerService;
     private ObjectReconstructor objectReconstructor;
-    private CanonicalJsonValidator canonicalizationValidator;
 
     @Inject
-    public DataUpload(ViewFactory viewFactory, RegisterService registerService, ObjectReconstructor objectReconstructor, CanonicalJsonValidator canonicalizationValidator) {
+    public DataUpload(ViewFactory viewFactory, RegisterService registerService, ObjectReconstructor objectReconstructor) {
         this.viewFactory = viewFactory;
         this.registerService = registerService;
         this.objectReconstructor = objectReconstructor;
-        this.canonicalizationValidator = canonicalizationValidator;
     }
 
     @Context
@@ -49,39 +44,18 @@ public class DataUpload {
     @Path("/load")
     public void load(String payload) {
         try {
-            String[] jsonLines = payload.split("\n");
-            mintItems(jsonLines, true);
+            Iterable<JsonNode> objects = objectReconstructor.reconstruct(payload.split("\n"));
+            mintItems(objects);
         } catch (Throwable t) {
             logger.error(Throwables.getStackTraceAsString(t));
             throw t;
         }
     }
 
-    @POST
-    @PermitAll
-    @Path("/loadCanonicalData")
-    public void loadCanonicalData(String payload) {
-        try {
-            String[] jsonLines = payload.split("\n");
-            mintItems(jsonLines, false);
-        } catch (Throwable t) {
-            logger.error(Throwables.getStackTraceAsString(t));
-            throw t;
-        }
-    }
-
-    private void mintItems(String[] jsonLines, boolean canonicalizeItems) {
+    private void mintItems(Iterable<JsonNode> objects) {
         registerService.asAtomicRegisterOperation(register -> {
-            Iterable<String> jsonLinesIterable = Arrays.asList(jsonLines);
-
-            if (!canonicalizeItems) {
-                // Check that item strings are in canonical form
-                jsonLinesIterable.forEach(l -> canonicalizationValidator.validateItemStringIsCanonicalized(l));
-            }
-
             AtomicInteger currentEntryNumber = new AtomicInteger(register.getTotalEntries());
-            Function<String, JsonNode> reconstructionFn = canonicalizeItems ? objectReconstructor::reconstruct : objectReconstructor::reconstructWithoutCanonicalization;
-            Iterables.transform(jsonLinesIterable, l -> new Item(reconstructionFn.apply(l))).forEach(item -> mintItem(register, currentEntryNumber, item));
+            Iterables.transform(objects, Item::new).forEach(item -> mintItem(register, currentEntryNumber, item));
         });
     }
 
