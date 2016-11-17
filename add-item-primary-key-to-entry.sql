33d1b145b5388f249dbcf403769e17e198c78e07
Chris Duraj <christopherduraj@gmail.com>
2016-11-17 11:18:10 +0000

    Migration script to create item_key field with appropriate value for Entry table

---
+++ add-item-primary-key-to-entry.sql
@@ -0,0 +1,24 @@
+create or replace function add_item_key_column(item_key_column text) returns void as $$
+BEGIN
+    IF item_key_column is NULL or item_key_column='' then
+        RAISE EXCEPTION 'Item key cannot be null or empty';
+    end if;
+
+    IF not exists(select 1 from item where content ? item_key_column) then
+        RAISE EXCEPTION 'Item content does not have key: %', item_key_column;
+    end if;
+
+    alter table entry add column if not exists item_key varchar;
+
+    IF EXISTS(select 1 from entry where not exists(select 1 from item where item.content->>item_key_column=entry.item_key))
+    THEN
+        update entry set item_key = content->>item_key_column from item where entry.sha256hex=item.sha256hex;
+    ELSE
+        RAISE NOTICE 'Column % exist and everything is fine', item_key_column;
+    END IF;
+
+END;
+$$ language plpgsql;
+
+select add_item_key_column(:item_key);
+drop function if exists add_item_key_column(item_key_column text);
