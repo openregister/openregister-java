398532580e762e35651756d09a55691b0f6012d6
Chris Duraj <christopherduraj@gmail.com>
2016-11-17 15:35:18 +0000

    Add item key column to entry table in schema scripts

--- create_schema.sh
+++ create_schema.sh
@@ -7,7 +7,7 @@ psql $db_name -U postgres -q -S -c "
 create table if not exists total_records(count INTEGER);
 create table if not exists total_entries(count INTEGER, last_updated TIMESTAMP WITHOUT TIME ZONE DEFAULT now());
 create table if not exists item (sha256hex varchar primary key, content jsonb);
-create table if not exists entry (entry_number integer primary key, sha256hex varchar, timestamp INTEGER);
+create table if not exists entry (entry_number integer primary key, sha256hex varchar, timestamp INTEGER, item_key varchar);
 create table if not exists current_keys(key VARCHAR PRIMARY KEY, entry_number INTEGER UNIQUE);
 create table if not exists current_entry_number(value integer not null);
 
