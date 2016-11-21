3f3e3707863cb25bd91642f99b79bddbce7a9bd6
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-11-21 15:03:18 +0000

    add test for analytics code endpoint

--- src/test/java/uk/gov/register/resources/HomePageResourceTest.java
+++ src/test/java/uk/gov/register/resources/HomePageResourceTest.java
@@ -9,6 +9,8 @@ import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
 
 public class HomePageResourceTest {
@@ -34,6 +36,30 @@ public class HomePageResourceTest {
         verify(registerMock, times(1)).getTotalRecords();
         verify(registerMock, times(1)).getTotalEntries();
         verify(viewFactoryMock, times(1)).homePageView(totalRecords, totalEntries, lastUpdated);
+    }
+
+    @Test
+    public void shouldRenderAnalyticsCodeIfPresent() throws Exception {
+        RegisterReadOnly registerMock = mock(RegisterReadOnly.class);
+        ViewFactory viewFactoryMock = mock(ViewFactory.class);
+
+        HomePageResource homePageResource = new HomePageResource(registerMock, viewFactoryMock, () -> Optional.of("codeForTest"));
+
+        String s = homePageResource.analyticsTrackingId();
+
+        assertThat(s, equalTo("var gaTrackingId = \"codeForTest\";\n"));
+    }
+
+    @Test
+    public void shouldRenderEmptyJsFileIfCodeIsAbsent() throws Exception {
+        RegisterReadOnly registerMock = mock(RegisterReadOnly.class);
+        ViewFactory viewFactoryMock = mock(ViewFactory.class);
+
+        HomePageResource homePageResource = new HomePageResource(registerMock, viewFactoryMock, () -> Optional.empty());
+
+        String s = homePageResource.analyticsTrackingId();
+
+        assertThat(s, equalTo(""));
     }
 }
 
