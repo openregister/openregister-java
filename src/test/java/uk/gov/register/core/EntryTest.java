c0819b4ebbdafeb9aaad38738a32ba4d2a1d5cb2
Chris Duraj <christopherduraj@gmail.com>
2016-11-22 12:27:24 +0000

    Make EntryTest clearer by specifying different key value

--- src/test/java/uk/gov/register/core/EntryTest.java
+++ src/test/java/uk/gov/register/core/EntryTest.java
@@ -8,25 +8,25 @@ import static org.junit.Assert.assertThat;
 public class EntryTest {
     @Test
     public void getTimestampAsISOFormat_returnsTheFormattedTimestamp() {
-        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "123");
+        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "sample-key");
         assertThat(entry.getTimestampAsISOFormat(), equalTo("2016-08-05T13:24:00Z"));
     }
 
     @Test
     public void getSha256hex_returnsTheSha256HexOfItem() {
-        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "123");
+        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "sample-key");
         assertThat(entry.getSha256hex(), equalTo("abc"));
     }
 
     @Test
     public void getItemHash_returnsSha256AsItemHash() {
-        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "123");
+        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "sample-key");
         assertThat(entry.getItemHash(), equalTo("sha-256:abc"));
     }
 
     @Test
     public void getkey_returnskeyValue() {
-        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "123");
-        assertThat(entry.getKey(), equalTo("123"));
+        Entry entry = new Entry(123, "abc", Instant.ofEpochSecond(1470403440), "sample-key");
+        assertThat(entry.getKey(), equalTo("sample-key"));
     }
 }
