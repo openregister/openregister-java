551e2468ce43749a4976c2079fb359fbed7bcbe4
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-11-24 13:58:18 +0000

    use flyway everywhere for schema management
    We have these scripts, create_schema.sh and drop_schema.sh, which are
    for managing database schemas.  We can now make them use the flyway
    database management commands instead of duplicating the same CREATE
    TABLE commands in different places.
    
    In other news, this commit also stops us creating the completely unused
    `total_entries` table! :tada:

--- create_schema.sh
+++ create_schema.sh
@@ -1,27 +1,7 @@
 #!/usr/bin/env bash
 
-db_name=$1
+set -eu
 
-psql $db_name -U postgres -q -S -c "
+config_file=$1
 
-create table if not exists total_records(count INTEGER);
-create table if not exists total_entries(count INTEGER, last_updated TIMESTAMP WITHOUT TIME ZONE DEFAULT now());
-create table if not exists item (sha256hex varchar primary key, content jsonb);
-create table if not exists entry (entry_number integer primary key, sha256hex varchar, timestamp INTEGER);
-create table if not exists current_keys(key VARCHAR PRIMARY KEY, entry_number INTEGER UNIQUE);
-create table if not exists current_entry_number(value integer not null);
-
-insert into total_records (count) select 0 where not exists (select count from total_records);
-insert into total_entries (count) select 0 where not exists (select count from total_entries);
-insert into current_entry_number(value)
-  select (
-    select case
-      when (select max(entry_number) from entry) is null then 0
-      else (select max(entry_number) from entry)
-    end as t
-  )
-  where not exists (
-    select 1 from current_entry_number
-  );
-
-"
+java -jar build/libs/openregister-java-all.jar db migrate $config_file
--- run-application.sh
+++ run-application.sh
@@ -2,7 +2,7 @@
 
 db_name=openregister_java
 
-sh create_schema.sh $db_name
+sh create_schema.sh config.yaml
 
 if [ `psql $db_name -U postgres -A -t -c "select count(*) from item"` -eq 0 ]; then
     ./gradlew loadSchoolData
