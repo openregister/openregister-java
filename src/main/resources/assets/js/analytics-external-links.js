188ed792c1297c64ed23b6b8377be9029bcac87e
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-12-01 10:48:59 +0000

    Revert "Enhance all the things - make sure that GA events are fired when navigating to external links or downloading the data on all browsers"
    This reverts commit 2630ba7d00ea1bfead91f1b97b635c14e3378390.

--- src/main/resources/assets/js/analytics-external-links.js
+++ src/main/resources/assets/js/analytics-external-links.js
@@ -1,15 +1,14 @@
 (function(){
-  var elems = document.getElementsByTagName('a');
-  var filtered = new Array();
+    var elems = document.getElementsByTagName('a');
+    var filtered = new Array();
 
-  for(var j = 0; j < elems.length; j++) {
-    var href = elems[j].getAttribute("href");
-    if (! (href.startsWith("/") || href.startsWith("#") || href.startsWith("mailto:")) ) {
-      filtered.push(elems[j]);
+    for(var j = 0; j < elems.length; j++) {
+        var href = elems[j].getAttribute("href");
+        if (! (href.startsWith("/") || href.startsWith("#") || href.startsWith("mailto:")) ) {
+            filtered.push(elems[j]);
+        }
     }
-  }
 
-  GOVUK.registers.analytics.setupEvent(filtered, "Link", "external", function(e){ 
-    return e.target.href; 
-  });
+    setupEventAnalytics(filtered, "Link", "external", function(e){ return e.target.href; });
 })();
+
