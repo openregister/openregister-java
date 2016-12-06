86c7bce8a2d20418e3dca91e7347a5969a68c2aa
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-12-06 10:58:45 +0000

    failing test to demonstrate problem with delete endpoint

--- src/test/java/uk/gov/register/functional/DeleteRegisterDataFunctionalTest.java
+++ src/test/java/uk/gov/register/functional/DeleteRegisterDataFunctionalTest.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.Response;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
@@ -38,5 +39,27 @@ public class DeleteRegisterDataFunctionalTest {
         String entriesRawJSON = entriesResponse2.readEntity(String.class);
 
         assertThat(entriesRawJSON, is("[]"));
+    }
+
+    @Test
+    public void deleteRegisterData_deletesProofCache() throws Exception {
+        String item1 = "{\"register\":\"register1\",\"text\":\"Register1 Text\", \"phase\":\"alpha\"}";
+        String item2 = "{\"register\":\"register2\",\"text\":\"Register2 Text\", \"phase\":\"alpha\"}";
+
+        register.deleteRegisterData();
+        register.mintLines(item1, item2);
+
+        Response proof1Response = register.getRequest("/proof/register/merkle:sha-256");
+        assertThat(proof1Response.getStatus(), equalTo(200));
+        String proof1 = proof1Response.readEntity(String.class);
+
+        register.deleteRegisterData();
+        register.mintLines(item2, item1);
+
+        Response proof2Response = register.getRequest("/proof/register/merkle:sha-256");
+        assertThat(proof2Response.getStatus(), equalTo(200));
+        String proof2 = proof2Response.readEntity(String.class);
+
+        assertThat(proof2, not(equalTo(proof1)));
     }
 }
