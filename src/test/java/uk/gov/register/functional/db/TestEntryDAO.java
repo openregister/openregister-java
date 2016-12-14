4800ad6de9ddd63c0e24eb9fd512fbad6eae811c
Michaela Benyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2016-12-14 12:37:10 +0000

    Add tests for EntryIteratorDAO

--- src/test/java/uk/gov/register/functional/db/TestEntryDAO.java
+++ src/test/java/uk/gov/register/functional/db/TestEntryDAO.java
@@ -1,12 +1,16 @@
 package uk.gov.register.functional.db;
 
+import org.skife.jdbi.v2.ResultIterator;
 import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.FetchSize;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
 import org.skife.jdbi.v2.tweak.ResultSetMapper;
 import uk.gov.register.core.Entry;
 import uk.gov.register.core.HashingAlgorithm;
+import uk.gov.register.db.mappers.EntryMapper;
 import uk.gov.register.util.HashValue;
 
 import java.sql.ResultSet;
@@ -23,6 +27,11 @@ public interface TestEntryDAO {
     @RegisterMapper(EntryMapper.class)
     @SqlQuery("select entry_number, sha256hex, timestamp, key from entry")
     List<Entry> getAllEntries();
+
+    @SqlQuery("SELECT * FROM entry WHERE entry_number >= :entryNumber ORDER BY entry_number")
+    @RegisterMapper(EntryMapper.class)
+    @FetchSize(262144) // Has to be non-zero to enable cursor mode https://jdbc.postgresql.org/documentation/head/query.html#query-with-cursor
+    ResultIterator<Entry> entriesIteratorFrom(@Bind("entryNumber") int entryNumber);
 
     class EntryMapper implements ResultSetMapper<Entry> {
         @Override
