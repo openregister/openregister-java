30a2e4ee19b03e7aa4a1b13287e9ab6b658cda28
Philip Potter <philip.potter@digital.cabinet-office.gov.uk>
2016-12-20 12:37:03 +0000

    use intension-revealing name

--- src/test/java/uk/gov/register/functional/HomePageFunctionalTest.java
+++ src/test/java/uk/gov/register/functional/HomePageFunctionalTest.java
@@ -15,20 +15,20 @@ import static org.junit.Assert.assertThat;
 
 public class HomePageFunctionalTest {
 
-    private final String registerName = "postcode";
+    private static final String REGISTER_WITH_COPYRIGHT_FIELD = "postcode";
 
     @ClassRule
     public static final RegisterRule register = new RegisterRule();
 
     @Test
     public void homePageIsAvailableWhenNoDataInRegister() {
-        Response response = register.getRequest(registerName, "/");
+        Response response = register.getRequest(REGISTER_WITH_COPYRIGHT_FIELD, "/");
         assertThat(response.getStatus(), equalTo(200));
     }
 
     @Test
     public void homepageHasXhtmlLangAttributes() throws Throwable {
-        Response response = register.getRequest(registerName, "/");
+        Response response = register.getRequest(REGISTER_WITH_COPYRIGHT_FIELD, "/");
 
         Document doc = Jsoup.parse(response.readEntity(String.class));
         Elements htmlElement = doc.select("html");
@@ -41,7 +41,7 @@ public class HomePageFunctionalTest {
     public void homepageHasCopyrightInMainBodyRenderedAsMarkdown() throws Throwable {
         // assumes that registers.yaml has a `postcode` entry with a copyright field containing markdown links
         // might be good to find a way to specify this in the test
-        Response response = register.getRequest(registerName, "/");
+        Response response = register.getRequest(REGISTER_WITH_COPYRIGHT_FIELD, "/");
         Document doc = Jsoup.parse(response.readEntity(String.class));
 
         Elements copyrightParagraph = doc.select("main .registry-copyright");
@@ -53,7 +53,7 @@ public class HomePageFunctionalTest {
     public void homepageHasCopyrightInFooterRenderedAsMarkdown() throws Throwable {
         // assumes that registers.yaml has a `postcode` entry with a copyright field containing markdown links
         // might be good to find a way to specify this in the test
-        Response response = register.getRequest(registerName, "/");
+        Response response = register.getRequest(REGISTER_WITH_COPYRIGHT_FIELD, "/");
         Document doc = Jsoup.parse(response.readEntity(String.class));
 
         Elements copyrightParagraph = doc.select("footer .registry-copyright");
