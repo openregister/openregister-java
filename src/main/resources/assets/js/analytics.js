7913c34b0bdcaac4c953209dc7d293d8f7fdc30b
Dan Mitchell <dan@digitalreflow.co.uk>
2016-12-22 15:44:24 +0000

    Fix whitespace and quotes in js files

--- src/main/resources/assets/js/analytics.js
+++ src/main/resources/assets/js/analytics.js
@@ -5,20 +5,20 @@ GOVUK.registers.analytics = (function () {
   var setupGA, setupEvents, sendPageview, sendEvent;
 
   var trackerId;
-  var isTrackerPresent = function(){
+  var isTrackerPresent = function() {
     return (typeof trackerId !== 'undefined') && (trackerId !== '');
   };
 
-  var checkTrackerAndExecute = function(fnToExecute, errorMsg){
-    if(isTrackerPresent()){
-        fnToExecute();
+  var checkTrackerAndExecute = function(fnToExecute, errorMsg) {
+    if (isTrackerPresent()) {
+      fnToExecute();
     } else {
-      console.error("GA tracker not present. " + errorMsg || "");
+      console.error('GA tracker not present. ' + errorMsg || '');
     }
   };
 
-  sendEvent = function(category, action, targetLabel, fnHitCallback){
-    checkTrackerAndExecute(function(){
+  sendEvent = function(category, action, targetLabel, fnHitCallback) {
+    checkTrackerAndExecute(function() {
       callbackExecuted = false;
       var executeHitCallback = function () {
         if (!callbackExecuted) {
@@ -29,44 +29,44 @@ GOVUK.registers.analytics = (function () {
       setTimeout(executeHitCallback, 1000);
 
       ga('send', 'event', category, action, targetLabel, { hitCallback: executeHitCallback });
-    }, "Cannot send event.")
+    }, 'Cannot send event.')
   };
 
-  sendPageview = function(){
-    checkTrackerAndExecute(function(){
+  sendPageview = function() {
+    checkTrackerAndExecute(function() {
       ga('send', 'pageview');
-    }, "Cannot send pageview.");
+    }, 'Cannot send pageview.');
   };
 
   setupGA = function(trackingId) {
     trackerId = trackingId;
-    checkTrackerAndExecute(function(){
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    checkTrackerAndExecute(function() {
+      (function(i,s,o,g,r,a,m) { i['GoogleAnalyticsObject']=r;i[r]=i[r]||function() {
+        (i[r].q=i[r].q||[]).push(arguments)}, i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0]; a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js','ga');
 
       ga('create', trackerId, 'auto');
-    }, "Cannot initialise GA.");
+    }, 'Cannot initialise GA.');
   };
 
-  setupEvent = function(elems, category, action, fnLabel){
-    checkTrackerAndExecute(function(){
-      for(var i=0; i < elems.length; i++) {
-        elems[i].addEventListener("click", function(e){
+  setupEvent = function(elems, category, action, fnLabel) {
+    checkTrackerAndExecute(function() {
+      for (var i=0; i < elems.length; i++) {
+        elems[i].addEventListener('click', function(e) {
           e.preventDefault();
           var targetLabel = fnLabel(e);
-          sendEvent(category, action, targetLabel, function(){ 
-            if(e.target.href){
-              document.location = e.target.href; 
+          sendEvent(category, action, targetLabel, function() {
+            if (e.target.href) {
+              document.location = e.target.href;
             } else {
-              console.log("GA event sent for target without href");
+              console.log('GA event sent for target without href');
               console.log(e);
             }
           });
         }, false);
       };
-    }, "Cannot setup event");
+    }, 'Cannot setup event');
   };
 
   return {
