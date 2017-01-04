8329674e0550f0fc92b5f602134676a3d37636d9
Dan Mitchell <dan@digitalreflow.co.uk>
2017-01-04 15:58:11 +0000

    Redesign homepage and standardise page structure
    Whilst redesigning the homepage there was a few inconsistencies with
    the page structure throughout the app so I updated throughout,
    including changes from the gov_toolkit, gov_elements and gov_components

--- src/test/java/uk/gov/register/functional/HomePageFunctionalTest.java
+++ src/test/java/uk/gov/register/functional/HomePageFunctionalTest.java
@@ -40,25 +40,13 @@ public class HomePageFunctionalTest {
     }
 
     @Test
-    public void homepageHasCopyrightInMainBodyRenderedAsMarkdown() throws Throwable {
-        // assumes that registers.yaml has a `postcode` entry with a copyright field containing markdown links
-        // might be good to find a way to specify this in the test
-        Response response = register.getRequest(REGISTER_WITH_COPYRIGHT_FIELD, "/");
-        Document doc = Jsoup.parse(response.readEntity(String.class));
-
-        Elements copyrightParagraph = doc.select("main .registry-copyright");
-        Elements links = copyrightParagraph.select("a");
-        assertThat(links.size(), greaterThan(0));
-    }
-
-    @Test
     public void homepageHasCopyrightInFooterRenderedAsMarkdown() throws Throwable {
         // assumes that registers.yaml has a `postcode` entry with a copyright field containing markdown links
         // might be good to find a way to specify this in the test
         Response response = register.getRequest(REGISTER_WITH_COPYRIGHT_FIELD, "/");
         Document doc = Jsoup.parse(response.readEntity(String.class));
 
-        Elements copyrightParagraph = doc.select("footer .registry-copyright");
+        Elements copyrightParagraph = doc.select("footer .copyright");
         Elements links = copyrightParagraph.select("a");
         assertThat(links.size(), greaterThan(0));
     }
