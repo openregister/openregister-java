fccc1f6360042fb0c9a75d290087b3a4bd59e0cd
John Ollier <john.ollier@digital.cabinet-office.gov.uk>
2017-03-10 13:59:11 +0000

    Allow parsing of RSF with multiple item hashes per entry.

--- src/main/java/uk/gov/register/serialization/handlers/AppendEntryCommandHandler.java
+++ src/main/java/uk/gov/register/serialization/handlers/AppendEntryCommandHandler.java
@@ -1,15 +1,18 @@
 package uk.gov.register.serialization.handlers;
 
+import com.google.common.base.Splitter;
 import uk.gov.register.core.Entry;
-import uk.gov.register.core.HashingAlgorithm;
 import uk.gov.register.core.Register;
-import uk.gov.register.serialization.RegisterResult;
 import uk.gov.register.serialization.RegisterCommand;
 import uk.gov.register.serialization.RegisterCommandHandler;
+import uk.gov.register.serialization.RegisterResult;
 import uk.gov.register.util.HashValue;
 
 import java.time.Instant;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+import static uk.gov.register.core.HashingAlgorithm.SHA256;
 
 public class AppendEntryCommandHandler extends RegisterCommandHandler {
     @Override
@@ -17,7 +20,9 @@ public class AppendEntryCommandHandler extends RegisterCommandHandler {
         try {
             List<String> parts = command.getCommandArguments();
             int newEntryNo = register.getTotalEntries() + 1;
-            Entry entry = new Entry(newEntryNo, HashValue.decode(HashingAlgorithm.SHA256, parts.get(1)), Instant.parse(parts.get(0)), parts.get(2));
+            List<HashValue> hashValues = Splitter.on(";").splitToList(parts.get(1)).stream()
+                    .map(h -> HashValue.decode(SHA256, h)).collect(toList());
+            Entry entry = new Entry(newEntryNo, hashValues, Instant.parse(parts.get(0)), parts.get(2));
             register.appendEntry(entry);
             return RegisterResult.createSuccessResult();
         } catch (Exception e) {
