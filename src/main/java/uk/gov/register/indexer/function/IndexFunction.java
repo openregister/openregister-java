09a9358849894dbeb9c2662796ec5a6ccde406ff
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2017-07-26 11:00:54 +0100

    Change index functions to use a read only register
    Currently, index functions take in their `execute` method a `Register`, which is the interface for a register which supports both reads and writes. Given that index functions only need to evaluate the current state of a register against some particular logic, there is no need for them to be given write access, so this commit changes all index functions to instead accept a `RegisterReadOnly`.

--- src/main/java/uk/gov/register/indexer/function/IndexFunction.java
+++ src/main/java/uk/gov/register/indexer/function/IndexFunction.java
@@ -1,12 +1,12 @@
 package uk.gov.register.indexer.function;
 
 import uk.gov.register.core.Entry;
-import uk.gov.register.core.Register;
+import uk.gov.register.core.RegisterReadOnly;
 import uk.gov.register.indexer.IndexKeyItemPair;
 
 import java.util.Set;
 
 public interface IndexFunction {
-    Set<IndexKeyItemPair> execute(Register register, Entry entry);
+    Set<IndexKeyItemPair> execute(RegisterReadOnly register, Entry entry);
     String getName();
 }
--- src/main/java/uk/gov/register/indexer/function/LocalAuthorityByTypeIndexFunction.java
+++ src/main/java/uk/gov/register/indexer/function/LocalAuthorityByTypeIndexFunction.java
@@ -1,7 +1,7 @@
 package uk.gov.register.indexer.function;
 
 import uk.gov.register.core.EntryType;
-import uk.gov.register.core.Register;
+import uk.gov.register.core.RegisterReadOnly;
 import uk.gov.register.indexer.IndexKeyItemPair;
 import uk.gov.register.util.HashValue;
 
@@ -14,7 +14,7 @@ public class LocalAuthorityByTypeIndexFunction extends BaseIndexFunction {
     }
 
     @Override
-    protected void execute(Register register, EntryType type, String key, HashValue itemHash, Set<IndexKeyItemPair> result) {
+    protected void execute(RegisterReadOnly register, EntryType type, String key, HashValue itemHash, Set<IndexKeyItemPair> result) {
         register.getItemBySha256(itemHash).ifPresent(i -> {
             if (i.getValue("local-authority-type").isPresent()) {
                 result.add(new IndexKeyItemPair(i.getValue("local-authority-type").get(), i.getSha256hex()));
