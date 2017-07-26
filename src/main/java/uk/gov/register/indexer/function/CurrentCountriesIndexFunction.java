7c053fa4549b7adca699c912fd10a5c188a03aae
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2017-07-26 11:00:54 +0100

    Change index functions to use a read only register
    Currently, index functions take in their `execute` method a `Register`, which is the interface for a register which supports both reads and writes. Given that index functions only need to evaluate the current state of a register against some particular logic, there is no need for them to be given write access, so this commit changes all index functions to instead accept a `RegisterReadOnly`.

--- src/main/java/uk/gov/register/indexer/function/CurrentCountriesIndexFunction.java
+++ src/main/java/uk/gov/register/indexer/function/CurrentCountriesIndexFunction.java
@@ -1,8 +1,9 @@
 package uk.gov.register.indexer.function;
 
 import uk.gov.register.core.EntryType;
-import uk.gov.register.core.Register;
+import uk.gov.register.core.RegisterReadOnly;
 import uk.gov.register.indexer.IndexKeyItemPair;
+import uk.gov.register.core.RegisterReadOnly;
 import uk.gov.register.util.HashValue;
 
 import java.util.Set;
@@ -14,7 +15,7 @@ public class CurrentCountriesIndexFunction extends BaseIndexFunction {
     }
 
     @Override
-    protected void execute(Register register, EntryType type, String key, HashValue itemHash, Set<IndexKeyItemPair> result) {
+    protected void execute(RegisterReadOnly register, EntryType type, String key, HashValue itemHash, Set<IndexKeyItemPair> result) {
         register.getItemBySha256(itemHash).ifPresent(i -> {
             if (!i.getValue("end-date").isPresent()) {
                 result.add(new IndexKeyItemPair(key, i.getSha256hex()));
