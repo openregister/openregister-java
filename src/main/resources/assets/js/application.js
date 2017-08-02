8d8c87024f7ec9e4c045b10019b79dded2716482
Dan Mitchell <dan@digitalreflow.co.uk>
2017-08-02 15:31:13 +0100

    Add role to button link

---
+++ src/main/resources/assets/js/application.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
+  // with role="button" when the space key is pressed.
+  GOVUK.shimLinksWithButtonRole.init();
+});
