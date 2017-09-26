5e246fc6a60783b1ce110fbcda28218cd38937da
fjavierm <javier.marquez@digital.cabinet-office.gov.uk>
2017-09-26 10:57:38 +0100

    Check that exceptions from ItemConverter do not affect all registers

--- src/main/java/uk/gov/register/service/ItemConverter.java
+++ src/main/java/uk/gov/register/service/ItemConverter.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.google.common.collect.Iterables;
 import org.jvnet.hk2.annotations.Service;
 import uk.gov.register.core.*;
+import uk.gov.register.exceptions.FieldConversionException;
 
 import java.util.Map;
 
@@ -13,18 +14,18 @@ import static uk.gov.register.core.Cardinality.ONE;
 @Service
 public class ItemConverter {
 
-    public Map<String, FieldValue> convertItem(Item item, final Map<String, Field> fieldsByName) {
+    public Map<String, FieldValue> convertItem(final Item item, final Map<String, Field> fieldsByName) {
         return item.getFieldsStream().collect(toMap(Map.Entry::getKey, e -> convert(e, fieldsByName)));
     }
 
-    FieldValue convert(Map.Entry<String, JsonNode> fieldNameToJson, Map<String, Field> fieldsByName) {
-        String fieldName = fieldNameToJson.getKey();
-        JsonNode value = fieldNameToJson.getValue();
+    FieldValue convert(final Map.Entry<String, JsonNode> fieldNameToJson, final Map<String, Field> fieldsByName) {
+        final String fieldName = fieldNameToJson.getKey();
+        final JsonNode value = fieldNameToJson.getValue();
         return convert(value, fieldsByName.get(fieldName));
     }
 
-    private FieldValue convert(JsonNode propertyJson, final Field field) {
-        Cardinality cardinality = field.getCardinality();
+    private FieldValue convert(final JsonNode propertyJson, final Field field) {
+        final Cardinality cardinality = field.getCardinality();
         if (cardinality == ONE) {
             return convertScalar(propertyJson, field);
         } else {
@@ -32,18 +33,25 @@ public class ItemConverter {
         }
     }
 
-    private FieldValue convertScalar(JsonNode value, Field field) {
-        if (field.getDatatype().getName().equals("curie")) {
-            if (value.textValue().contains(":")) {
-                return new LinkValue.CurieValue(value.textValue());
+    private FieldValue convertScalar(final JsonNode value, final Field field) {
+        try {
+            if (field.getDatatype().getName().equals("curie")) {
+                if (value.textValue().contains(":")) {
+                    return new LinkValue.CurieValue(value.textValue());
+                }
+
+                return new LinkValue(field.getRegister().get(), value.textValue());
+            } else if (field.getRegister().isPresent()) {
+                return new LinkValue(field.getRegister().get(), value.textValue());
+                //Note: the equals check below must be replaced with the specified datatype, instead of doing string comparision
+                // We should replace this once the datatype register is available
+            } else {
+                return new StringValue(value.textValue());
             }
-            return new LinkValue(field.getRegister().get(), value.textValue());
-        } else if (field.getRegister().isPresent()) {
-            return new LinkValue(field.getRegister().get(), value.textValue());
-            //Note: the equals check below must be replaced with the specified datatype, instead of doing string comparision
-            // We should replace this once the datatype register is available
-        } else {
-            return new StringValue(value.textValue());
+        } catch (final Exception exception) {
+            throw new FieldConversionException(
+                    String.format("Not possible to process the field (name, data-type, value): %s, %s, %s.",
+                            field.fieldName, field.getDatatype().getName(), value.textValue()));
         }
     }
 
