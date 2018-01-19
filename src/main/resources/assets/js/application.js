612814abdfe61eb78a73459e1b5f2e79f0aed909
Dan Mitchell <dan@digitalreflow.co.uk>
2018-01-19 14:17:41 +0000

    Forgot to fire gaap analytics tracking

--- src/main/resources/assets/js/application.js
+++ src/main/resources/assets/js/application.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
   // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
   // with role="button" when the space key is pressed.
   GOVUK.shimLinksWithButtonRole.init();
+
+  // Fire GaaP analytics
+  window.GAAP.analytics.init();
 });
