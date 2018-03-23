78d21458646c6fcdf34386d481cdfbebef98c601
michaelabenyohai <michaela.benyohai@digital.cabinet-office.gov.uk>
2018-03-23 15:32:15 +0000

    Add PeriodDatatype as per http://openregister.github.io/specification/#period-datatype

--- src/main/java/uk/gov/register/util/DateFormatChecker.java
+++ src/main/java/uk/gov/register/util/DateFormatChecker.java
@@ -4,22 +4,37 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 
 import java.text.ParseException;
+import java.util.Date;
 
 import static uk.gov.register.util.DateFormat.ISO_8601.*;
 
 public class DateFormatChecker {
 
-    public static boolean isDateFormatValid(final String dateToValidate) {
+    public static boolean isDateTimeFormatValid(String dateToValidate) {
         if (StringUtils.isEmpty(dateToValidate)) {
-            return true;
+            return false;
         }
 
         try {
-            DateUtils.parseDate(dateToValidate, YEAR, YEAR_MONTH, YEAR_MONTH_DAY, YEAR_MONTH_DAY_HOURS_MINUTES_SECONDS, YEAR_MONTH_DAY_HOURS_MINUTES_SECONDS_UTC);
-
+            parseDateTime(dateToValidate);
             return true;
-        } catch (ParseException e) {
+        } catch (ParseException ex) {
             return false;
         }
+    }
+
+    public static boolean isDateTimeFormatsOrdered(String startString, String endString) {
+        try {
+            Date start = parseDateTime(startString);
+            Date end = parseDateTime(endString);
+
+            return start.before(end);
+        } catch (ParseException ex) {
+            return false;
+        }
+    }
+
+    private static Date parseDateTime(String dateTimeString) throws ParseException {
+        return DateUtils.parseDate(dateTimeString, YEAR, YEAR_MONTH, YEAR_MONTH_DAY, YEAR_MONTH_DAY_HOURS_MINUTES_SECONDS, YEAR_MONTH_DAY_HOURS_MINUTES_SECONDS_UTC);
     }
 }
