e1bdca78043b901da9a48628cc5386e355b93989
Mat Moore <matmoore@users.noreply.github.com>
2018-07-17 16:47:55 +0100

    Add response headers to announce deprecated formats
    We are planning to drop support for all representations except JSON
    and CSV.
    
    This adds:
    
    - Link: <https://....json>; rel="alternate"; type="application/json" for .yaml or .ttl
    - Link: <https://....csv>; rel="alternate"; type="text/csv" for any .tsv or .xlsx
    - Warning: 299 - "Miscellaneous Persistent Warning" "yaml is deprecated
      and will be removed. See "Link" header for a format to use instead." for all deprecated formats

---
+++ src/main/java/uk/gov/register/filters/DeprecatedFormatFilter.java
@@ -0,0 +1,91 @@
+package uk.gov.register.filters;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+import java.net.URI;
+import java.util.Optional;
+
+import com.google.common.net.HttpHeaders;
+import uk.gov.register.views.representations.ExtraMediaType;
+
+@Provider
+public class DeprecatedFormatFilter implements ContainerResponseFilter {
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+        final UriInfo uriInfo = requestContext.getUriInfo();
+        final URI requestedUri = uriInfo.getRequestUri();
+        final MultivaluedMap<String, Object> headers = responseContext.getHeaders();
+
+        extractMediaType(headers).ifPresent(mediaType -> filterMediaType(mediaType, requestedUri, headers));
+    }
+
+    private void filterMediaType(MediaType mediaType, URI requestedUri, MultivaluedMap<String, Object> headers) {
+        final String alternateUri;
+        final String alternateType;
+
+        if (recommendJson(mediaType)) {
+            alternateUri = alternateLink(requestedUri, ".json");
+            alternateType = MediaType.APPLICATION_JSON;
+        } else if (recommendCsv(mediaType)) {
+            alternateUri = alternateLink(requestedUri, ".csv");
+            alternateType = ExtraMediaType.TEXT_CSV;
+        } else {
+            return;
+        }
+
+        headers.add(
+                HttpHeaders.LINK,
+                linkHeader(alternateType, alternateUri)
+        );
+        headers.add(
+                HttpHeaders.WARNING,
+                warningHeader(mediaType.getSubtype())
+        );
+    }
+
+    private String alternateLink(URI requestedUri, String recommendedExtension) {
+        final String path = requestedUri.getPath();
+        final UriBuilder builder = UriBuilder.fromUri(requestedUri);
+
+        // Even if an extension was specified in the original URI, it has been stripped out
+        // at this point, so just add the new one (before any trailing slash).
+        final String newPath = path.replaceFirst("(/)?$", recommendedExtension + "$1");
+
+        builder.replacePath(newPath);
+
+        return builder.toTemplate();
+    }
+
+    private String linkHeader(String alternateType, String alternateUri) {
+        return alternateUri + "; rel=\"alternate\"" + "; type=\"" + alternateType + "\"";
+    }
+
+    private String warningHeader(String deprecatedType) {
+        return "299 - \"Miscellaneous Persistent Warning\" \"" + deprecatedType + " is deprecated and will be removed. See \"Link\" header for a format to use instead.\"";
+    }
+
+    private boolean recommendJson(MediaType mediaType) {
+        return mediaType.equals(ExtraMediaType.TEXT_TTL_TYPE) ||
+                mediaType.equals(ExtraMediaType.TEXT_YAML_TYPE);
+    }
+
+    private boolean recommendCsv(MediaType mediaType) {
+        return mediaType.equals(ExtraMediaType.TEXT_TSV_TYPE) ||
+                mediaType.equals(ExtraMediaType.APPLICATION_SPREADSHEET_TYPE);
+    }
+
+    private Optional<MediaType> extractMediaType(MultivaluedMap<String, Object> headers) {
+        try {
+            MediaType mediaType = (MediaType) headers.getFirst(HttpHeaders.CONTENT_TYPE);
+            return Optional.ofNullable(mediaType);
+        } catch (ClassCastException e) {
+            return Optional.empty();
+        }
+    }
+}
---
+++ src/test/java/uk/gov/register/filters/DeprecatedFormatFilterTest.java
@@ -0,0 +1,77 @@
+package uk.gov.register.filters;
+
+import io.dropwizard.testing.junit.ResourceTestRule;
+import org.junit.Rule;
+import org.junit.Test;
+import uk.gov.register.views.representations.ExtraMediaType;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+public class DeprecatedFormatFilterTest {
+    @Rule
+    public ResourceTestRule rule = ResourceTestRule.builder().addResource(new DeprecatedFormatFilterTest.DummyResource()).addProvider(new DeprecatedFormatFilter()).build();
+
+    @Test
+    public void jsonResponseShouldNotWarn() {
+        Response response = rule.client().target("/json").request().get();
+
+        assertFalse(response.getStringHeaders().containsKey("Warning"));
+        assertFalse(response.getStringHeaders().containsKey("Link"));
+    }
+
+    @Test
+    public void yamlResponseShouldWarn() {
+        Response response = rule.client().target("/yaml").request().get();
+
+        String warning = response.getHeaderString("Warning");
+        String link = response.getHeaderString("Link");
+
+        assertThat(warning, equalTo("299 - \"Miscellaneous Persistent Warning\" \"yaml is deprecated and will be removed. See \"Link\" header for a format to use instead.\""));
+        assertThat(link, equalTo("/yaml.json; rel=\"alternate\"; type=\"application/json\""));
+    }
+
+    @Test
+    public void existingLinkHeadersShouldBePreserved() {
+        Response response = rule.client().target("/existing-header").request().get();
+
+        String link = response.getHeaderString("Link");
+
+        assertThat(link, equalTo("<?page-index=2&page-size=100>; rel=\"next\",/existing-header.json; rel=\"alternate\"; type=\"application/json\""));
+    }
+
+    @Path("/")
+    public class DummyResource {
+        @GET
+        @Path("json")
+        @Produces(MediaType.APPLICATION_JSON)
+        public String json() {
+            return "{}";
+        }
+
+        @GET
+        @Path("yaml")
+        @Produces(ExtraMediaType.TEXT_YAML)
+        public String yaml() {
+            return "{}";
+        }
+
+        @GET
+        @Path("existing-header")
+        public Response existingHeader() {
+            return Response
+                    .status(200)
+                    .header("Link", "<?page-index=2&page-size=100>; rel=\"next\"")
+                    .type(ExtraMediaType.TEXT_YAML)
+                    .entity("{}")
+                    .build();
+        }
+    }
+}
