7028793685f382fc651bfa1bdbe58576f62e7e99
Mat Moore <matmoore@users.noreply.github.com>
2018-10-15 14:09:07 +0100

    Add blob resource
    Blob is the new name for item. Version this URL at 2 from the
    beginning to indicate it's not a new thing.
    
    Some decisions here:
    
    - I'm currently versioning at the resource level, not the whole API level, since
      resources can evolve at different rates, and not all consumers need
      all endpoints
      - This means you can ignore breaking changes you don't care about
      - HOWEVER, changing the version of a resource requires a new version
        of any resources that link to it
      - So it may be simpler to just version the whole API anyway
    
    - In this case, the only difference is the URL, so it's safe to link to
      blobs OR items from the original version of /entries
    
    - Resources without a version number are implicitly version 1 of that
      resource
    
    - We may redirect /v1/FOO to /FOO throughout, but I haven't done this
      yet
    
    In all cases, the intent is to prevent client code from breaking
    unexpectedly - where we change the API, the client should opt in to the
    new version by changing the URL.

---
+++ src/main/java/uk/gov/register/resources/BlobResource.java
@@ -0,0 +1,59 @@
+package uk.gov.register.resources;
+
+import com.codahale.metrics.annotation.Timed;
+import uk.gov.register.core.HashingAlgorithm;
+import uk.gov.register.core.Item;
+import uk.gov.register.core.RegisterReadOnly;
+import uk.gov.register.exceptions.FieldConversionException;
+import uk.gov.register.util.HashValue;
+import uk.gov.register.views.AttributionView;
+import uk.gov.register.views.ItemView;
+import uk.gov.register.views.ViewFactory;
+import uk.gov.register.views.representations.ExtraMediaType;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.Optional;
+
+@Path("/v2/blobs")
+public class BlobResource {
+    private final RegisterReadOnly register;
+    private final ViewFactory viewFactory;
+
+    @Inject
+    public BlobResource(RegisterReadOnly register, ViewFactory viewFactory) {
+        this.register = register;
+        this.viewFactory = viewFactory;
+    }
+
+    @GET
+    @Path("/sha-256:{blob-hash}")
+    @Produces(ExtraMediaType.TEXT_HTML)
+    @Timed
+    public AttributionView<ItemView> getItemWebViewByHex(@PathParam("blob-hash") String blobHash) throws FieldConversionException {
+        return getBlob(blobHash).map(viewFactory::getItemView)
+                .orElseThrow(() -> new NotFoundException("No item found with hash: " + blobHash));
+    }
+
+    @GET
+    @Path("/sha-256:{blob-hash}")
+    @Produces({
+            MediaType.APPLICATION_JSON,
+            ExtraMediaType.TEXT_YAML,
+            ExtraMediaType.TEXT_TTL,
+            ExtraMediaType.TEXT_CSV,
+            ExtraMediaType.TEXT_TSV,
+            ExtraMediaType.APPLICATION_SPREADSHEET
+    })
+    @Timed
+    public ItemView getBlobDataByHex(@PathParam("blob-hash") String blobHash) throws FieldConversionException {
+        return getBlob(blobHash).map(viewFactory::getItemMediaView)
+                .orElseThrow(() -> new NotFoundException("No blob found with hash: " + blobHash));
+    }
+
+    private Optional<Item> getBlob(String blobHash) {
+        HashValue hash = new HashValue(HashingAlgorithm.SHA256, blobHash);
+        return register.getItem(hash);
+    }
+}
