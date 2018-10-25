22ef1b982b150624ce5f968ed9c4a6047ffc3572
Mat Moore <matmoore@users.noreply.github.com>
2018-10-25 17:09:42 +0100

    Add EntryView
    This contains all the serialization/deserialization functionality
    from Entry, so that the core Entry class no longer needs to handle
    this. This will allow us to start versioning the view layer (i.e.
    introduce multiple view classes for the same core class, without
    changing the persistence layers)

---
+++ src/main/java/uk/gov/register/views/EntryView.java
@@ -0,0 +1,90 @@
+package uk.gov.register.views;
+
+import com.fasterxml.jackson.annotation.*;
+import uk.gov.register.core.Entry;
+import uk.gov.register.util.HashValue;
+import uk.gov.register.util.ISODateFormatter;
+import uk.gov.register.util.ToArrayConverter;
+import uk.gov.register.views.CsvRepresentationView;
+import uk.gov.register.views.representations.CsvRepresentation;
+
+import java.time.Instant;
+import java.util.Iterator;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.google.common.collect.Lists;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonPropertyOrder({"index-entry-number", "entry-number", "entry-timestamp", "key", "item-hash"})
+public class EntryView implements CsvRepresentationView<EntryView> {
+    private final int entryNumber;
+    private final HashValue hashValue;
+    private final Instant timestamp;
+    private String key;
+
+    public EntryView(Entry entry) {
+        this.entryNumber = entry.getEntryNumber();
+        this.hashValue = entry.getItemHash();
+        this.timestamp = entry.getTimestamp();
+        this.key = entry.getKey();
+    }
+
+    @JsonIgnore
+    public Instant getTimestamp() {
+        return timestamp;
+    }
+
+    @JsonProperty("item-hash")
+    @JsonSerialize(using = ToArrayConverter.class)
+    public HashValue getItemHash() {
+        return hashValue;
+    }
+
+    @JsonProperty("entry-number")
+    @JsonSerialize(using = ToStringSerializer.class)
+    public Integer getEntryNumber() {
+        return entryNumber;
+    }
+
+    @JsonProperty("index-entry-number")
+    @JsonSerialize(using = ToStringSerializer.class)
+    public Integer getIndexEntryNumber() {
+        return entryNumber;
+    }
+
+    @JsonProperty("entry-timestamp")
+    public String getTimestampAsISOFormat() {
+        return ISODateFormatter.format(timestamp);
+    }
+
+    @JsonProperty("key")
+    public String getKey() {
+        return key;
+    }
+
+    public static CsvSchema csvSchema() {
+        CsvMapper csvMapper = new CsvMapper();
+        csvMapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
+        return csvMapper.schemaFor(uk.gov.register.core.Entry.class);
+    }
+
+    public static CsvSchema csvSchemaWithOmittedFields(List<String> fieldsToRemove) {
+        CsvSchema originalSchema = csvSchema();
+        Iterator<CsvSchema.Column> columns = originalSchema.rebuild().getColumns();
+
+        List<CsvSchema.Column> updatedColumns = Lists.newArrayList(columns);
+        updatedColumns.removeIf(c -> fieldsToRemove.contains(c.getName()));
+
+        return CsvSchema.builder().addColumns(updatedColumns).build();
+    }
+
+    @Override
+    public CsvRepresentation<EntryView> csvRepresentation() {
+        return new CsvRepresentation<>(csvSchema(), this);
+    }
+}
