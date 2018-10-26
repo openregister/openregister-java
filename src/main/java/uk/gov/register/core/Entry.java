a9002bb461f5e526fabe45b3759b21015c90f628
Mat Moore <matmoore@users.noreply.github.com>
2018-10-26 13:32:48 +0100

    Remove presentation code from Entry class
    This should no longer be used for serializing JSON/CSV etc

--- src/main/java/uk/gov/register/core/Entry.java
+++ src/main/java/uk/gov/register/core/Entry.java
@@ -1,26 +1,11 @@
 package uk.gov.register.core;
 
-import com.fasterxml.jackson.annotation.*;
 import uk.gov.register.util.HashValue;
 import uk.gov.register.util.ISODateFormatter;
-import uk.gov.register.util.ToArrayConverter;
-import uk.gov.register.views.CsvRepresentationView;
-import uk.gov.register.views.representations.CsvRepresentation;
 
 import java.time.Instant;
-import java.util.Iterator;
-import java.util.List;
 
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import com.google.common.collect.Lists;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonPropertyOrder({"index-entry-number", "entry-number", "entry-timestamp", "key", "item-hash"})
-public class Entry implements CsvRepresentationView<Entry> {
+public class Entry {
     private final int entryNumber;
     private final HashValue hashValue;
     private final Instant timestamp;
@@ -35,68 +20,36 @@ public class Entry implements CsvRepresentationView<Entry> {
         this.entryType = entryType;
     }
 
-    @JsonIgnore
     public Instant getTimestamp() {
         return timestamp;
     }
 
-    @JsonProperty("item-hash")
-    @JsonSerialize(using = ToArrayConverter.class)
     public HashValue getItemHash() {
         return hashValue;
     }
 
-    @JsonIgnore
     public long getTimestampAsLong() {
         return timestamp.getEpochSecond();
     }
 
-    @JsonProperty("entry-number")
-    @JsonSerialize(using = ToStringSerializer.class)
     public Integer getEntryNumber() {
         return entryNumber;
     }
 
-    @JsonProperty("index-entry-number")
-    @JsonSerialize(using = ToStringSerializer.class)
     public Integer getIndexEntryNumber() {
         return entryNumber;
     }
 
-    @JsonProperty("entry-timestamp")
     public String getTimestampAsISOFormat() {
         return ISODateFormatter.format(timestamp);
     }
 
-    @JsonProperty("key")
     public String getKey() {
         return key;
     }
 
-    @JsonIgnore
     public EntryType getEntryType() {
         return entryType;
-    }
-
-    public static CsvSchema csvSchema() {
-        CsvMapper csvMapper = new CsvMapper();
-        csvMapper.disable(MapperFeature.SORT_PROPERTIES_ALPHABETICALLY);
-        return csvMapper.schemaFor(Entry.class);
-    }
-
-    public static CsvSchema csvSchemaWithOmittedFields(List<String> fieldsToRemove) {
-        CsvSchema originalSchema = csvSchema();
-        Iterator<CsvSchema.Column> columns = originalSchema.rebuild().getColumns();
-
-        List<CsvSchema.Column> updatedColumns = Lists.newArrayList(columns);
-        updatedColumns.removeIf(c -> fieldsToRemove.contains(c.getName()));
-
-        return CsvSchema.builder().addColumns(updatedColumns).build();
-    }
-
-    @Override
-    public CsvRepresentation<Entry> csvRepresentation() {
-        return new CsvRepresentation<>(csvSchema(), this);
     }
 
     @Override
