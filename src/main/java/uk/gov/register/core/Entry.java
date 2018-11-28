38b515aa4c2ebe28be2ec7a94af5219f49210cc6
Mat Moore <matmoore@users.noreply.github.com>
2018-11-28 15:31:48 +0000

    Use blobHash for equality/hashing of Item/Entry objects
    I've removed both `v1ItemHash.hashCode()` and `content.hashCode()` as
    both `v1ItemHash` and `blobHash` are derived from `content`, so any
    one of them is sufficient to distinguish between blobs.

--- src/main/java/uk/gov/register/core/Entry.java
+++ src/main/java/uk/gov/register/core/Entry.java
@@ -80,7 +80,7 @@ public class Entry {
         if (key != null ? !key.equals(entry.key) : entry.key != null) return false;
         if (timestamp != null ? !timestamp.equals(entry.timestamp) : entry.timestamp != null) return false;
         if (entryType != null ? !entryType.equals(entry.entryType) : entry.entryType != null) return false;
-        return v1ItemHash != null ? v1ItemHash.equals(entry.v1ItemHash) : entry.v1ItemHash == null;
+        return blobHash != null ? blobHash.equals(entry.blobHash) : entry.blobHash == null;
     }
 
     @Override
@@ -90,7 +90,7 @@ public class Entry {
         result = 31 * result + (timestamp != null ? timestamp.hashCode() : 0);
         result = 31 * result + (key != null ? key.hashCode() : 0);
         result = 31 * result + (entryType != null ? entryType.hashCode() : 0);
-        result = 31 * result + (v1ItemHash != null ? v1ItemHash.hashCode() : 0);
+        result = 31 * result + (blobHash != null ? blobHash.hashCode() : 0);
 
         return result;
     }
