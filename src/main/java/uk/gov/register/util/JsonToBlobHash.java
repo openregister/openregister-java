b675e735067bc26e7f7994cb64f3ce9acdb4b8d1
Mat Moore <matmoore@users.noreply.github.com>
2018-11-29 11:07:09 +0000

    Support cardinality N case to item hash algorithm

--- src/main/java/uk/gov/register/util/JsonToBlobHash.java
+++ src/main/java/uk/gov/register/util/JsonToBlobHash.java
@@ -3,12 +3,15 @@ package uk.gov.register.util;
 import com.fasterxml.jackson.databind.JsonNode;
 import uk.gov.objecthash.ObjectHash;
 import uk.gov.objecthash.ObjectHashable;
+import uk.gov.objecthash.SetValue;
 import uk.gov.objecthash.StringValue;
 import uk.gov.register.core.HashingAlgorithm;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.stream.Collectors;
@@ -18,17 +21,25 @@ import java.util.stream.StreamSupport;
 public class JsonToBlobHash {
 
     public static HashValue apply (JsonNode jsonNode){
-
+        Map<String, ObjectHashable> data = new HashMap<>();
         Iterator<Map.Entry<String, JsonNode>> fields = jsonNode.fields();
         Stream<Map.Entry<String, JsonNode>> stream = StreamSupport.stream(Spliterators.spliteratorUnknownSize(fields, Spliterator.ORDERED), false);
         String result = stream.map(node -> {
             String key = node.getKey();
-                     Boolean valueIsArray = node.getValue().isArray();
-                    String value = node.getValue().asText();
-                    Map<String, ObjectHashable> data = new HashMap<>();
-                    data.put(key, new StringValue(value));
-                    return ObjectHash.toHexDigest(data);
-                }
+            Boolean valueIsArray = node.getValue().isArray();
+
+            if(valueIsArray) {
+                Set<ObjectHashable> values = new HashSet<>();
+                node.getValue().elements().forEachRemaining(value -> values.add(new StringValue(value.asText())));
+                SetValue setValue = new SetValue(values);
+                data.put(key, setValue);
+                return ObjectHash.toHexDigest(data);
+            } else {
+                String value = node.getValue().asText();
+                data.put(key, new StringValue(value));
+                return ObjectHash.toHexDigest(data);
+            }
+        }
         ).collect(Collectors.joining(""));
         return new HashValue(HashingAlgorithm.SHA256, result);
     }
--- src/test/java/uk/gov/register/util/JsonToBlobHashTest.java
+++ src/test/java/uk/gov/register/util/JsonToBlobHashTest.java
@@ -12,10 +12,17 @@ import static org.junit.Assert.*;
 public class JsonToBlobHashTest {
 
     @Test
-    public void hashesJsonNode() throws IOException {
+    public void hashesJsonNodeWithCardinality1() throws IOException {
         JsonNode jsonNode = new ObjectMapper().readTree("{\"foo\": \"bar\"}");
 
         assertEquals("7ef5237c3027d6c58100afadf37796b3d351025cf28038280147d42fdc53b960", JsonToBlobHash.apply(jsonNode).getValue());
+    }
+
+    @Test
+    public void hashesJsonNodeWithCardinalityN() throws IOException {
+        JsonNode jsonNode = new ObjectMapper().readTree("{\"foo\": [\"bar\", \"baz\"]}");
+
+        assertEquals("e64c38241421a64d3cbe3e0f1a7b1a429bd7b9b5ce3bb4d0840f70ecee3cd460", JsonToBlobHash.apply(jsonNode).getValue());
     }
 
 }
