668b9313791cf6efa63dd05bf798034e62f8030c
Mat Moore <matmoore@users.noreply.github.com>
2018-12-05 14:07:51 +0000

    Make hash population a normal database migration
    Running a command in cloud foundry is harder than I thought, so just
    use our normal migration process. This may cause some downtime if the
    migration is slow to run, but we'll see how it behaves on test and alpha.

--- src/main/java/uk/gov/register/RegisterApplication.java
+++ src/main/java/uk/gov/register/RegisterApplication.java
@@ -22,7 +22,6 @@ import uk.gov.register.auth.BasicAuthFilter;
 import uk.gov.register.auth.RegisterAuthDynamicFeature;
 import uk.gov.register.configuration.*;
 import uk.gov.register.core.*;
-import uk.gov.register.commands.HashDataMigrationCommand;
 import uk.gov.register.db.Factories;
 import uk.gov.register.filters.CorsBundle;
 import uk.gov.register.filters.StripTrailingSlashRedirectFilter;
@@ -85,7 +84,6 @@ public class RegisterApplication extends Application<RegisterConfiguration> {
         bootstrap.addBundle(new AssetsBundle("/assets"));
         bootstrap.addBundle(new CorsBundle());
         bootstrap.addBundle(new LogstashBundle());
-        bootstrap.addCommand(new HashDataMigrationCommand(this));
 
         System.setProperty("java.protocol.handler.pkgs", "uk.gov.register.protocols");
     }
