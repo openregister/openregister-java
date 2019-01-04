8e146097c41b650c9f764ea990325a3dbdb553c8
Gideon Goldberg <gidsgoldberg@gmail.com>
2019-01-04 12:31:36 +0000

    update deprecated constructor

--- src/test/java/uk/gov/register/providers/JsonParseExceptionMapperTest.java
+++ src/test/java/uk/gov/register/providers/JsonParseExceptionMapperTest.java
@@ -14,7 +14,7 @@ public class JsonParseExceptionMapperTest {
     @Test
     public void toResponse_returns400Response() {
         JsonParseExceptionMapper jsonParseExceptionMapper = new JsonParseExceptionMapper();
-        Response response = jsonParseExceptionMapper.toResponse(new JsonParseException("some message", null));
+        Response response = jsonParseExceptionMapper.toResponse(new JsonParseException(null, "some message"));
 
         assertThat(response.getStatus(), equalTo(400));
         assertThat(response.getEntity(), equalTo("Error parsing json input: some message"));
